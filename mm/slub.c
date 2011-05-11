9939029b9cddd7ecd1153b80bd0b836c09ed9b3f
Mel Gorman <mgorman@suse.de>
2011-05-11 16:29:33 +0100

    mm: slub: Default slub_max_order to 0
    To avoid locking and per-cpu overhead, SLUB optimisically uses
    high-order allocations up to order-3 by default and falls back to
    lower allocations if they fail. While care is taken that the caller
    and kswapd take no unusual steps in response to this, there are
    further consequences like shrinkers who have to free more objects to
    release any memory. There is anecdotal evidence that significant time
    is being spent looping in shrinkers with insufficient progress being
    made (https://lkml.org/lkml/2011/4/28/361) and keeping kswapd awake.
    
    SLUB is now the default allocator and some bug reports have been
    pinned down to SLUB using high orders during operations like
    copying large amounts of data. SLUBs use of high-orders benefits
    applications that are sized to memory appropriately but this does not
    necessarily apply to large file servers or desktops.  This patch
    causes SLUB to use order-0 pages like SLAB does by default.
    There is further evidence that this keeps kswapd's usage lower
    (https://lkml.org/lkml/2011/5/10/383).
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- mm/slub.c
+++ mm/slub.c
@@ -3319,7 +3319,7 @@ EXPORT_SYMBOL(kmem_cache_alloc_bulk);
  * take the list_lock.
  */
 static int slub_min_order;
-static int slub_max_order = PAGE_ALLOC_COSTLY_ORDER;
+static int slub_max_order;
 static int slub_min_objects;
 
 /*
