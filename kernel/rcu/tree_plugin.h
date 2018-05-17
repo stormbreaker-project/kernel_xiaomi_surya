35d8afdcbc30af1e29313061f39e80a8c11da281
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2018-05-17 11:21:20 -0700

    rcu: Remove unused local variable "cpu"
    One danger of using __maybe_unused is that the compiler doesn't yell
    at you when you remove the last reference, witness rcu_bind_gp_kthread()
    and its local variable "cpu".  This commit removes this local variable.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/rcu/tree_plugin.h
+++ kernel/rcu/tree_plugin.h
@@ -2669,8 +2669,6 @@ static bool rcu_nohz_full_cpu(struct rcu_state *rsp)
  */
 static void rcu_bind_gp_kthread(void)
 {
-	int __maybe_unused cpu;
-
 	if (!tick_nohz_full_enabled())
 		return;
 	housekeeping_affine(current);
