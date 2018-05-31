dc7aca576b21be9b188444375c7bfe67d60dfd6f
Christoph Hellwig <hch@lst.de>
2018-05-31 19:11:38 +0200

    block: remove the always unused name argument to elevator_init
    Reported-by: Damien Le Moal <Damien.LeMoal@wdc.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Damien Le Moal <damien.lemoal@wdc.com>
    Tested-by: Damien Le Moal <damien.lemoal@wdc.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- block/blk.h
+++ block/blk.h
@@ -211,7 +211,7 @@ static inline void elv_deactivate_rq(struct request_queue *q, struct request *rq
 		e->type->ops.sq.elevator_deactivate_req_fn(q, rq);
 }
 
-int elevator_init(struct request_queue *, char *);
+int elevator_init(struct request_queue *);
 void elevator_exit(struct request_queue *, struct elevator_queue *);
 
 struct hd_struct *__disk_get_part(struct gendisk *disk, int partno);
