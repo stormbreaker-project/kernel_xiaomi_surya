8987954ee16b2e2e6ec744cbc4d98bf620410243
kdrag0n <dragon@khronodragon.com>
2018-12-27 20:46:40 -0800

    block: loop: set non-rotational queue flag
    All of the backing devices we will potentially be using loop on will be
    solid state.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -1868,6 +1868,8 @@ static int loop_add(struct loop_device **l, int i)
 	 */
 	queue_flag_set_unlocked(QUEUE_FLAG_NOMERGES, lo->lo_queue);
 
+	queue_flag_set_unlocked(QUEUE_FLAG_NONROT, lo->lo_queue);
+
 	err = -ENOMEM;
 	disk = lo->lo_disk = alloc_disk(1 << part_shift);
 	if (!disk)
