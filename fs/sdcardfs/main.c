e29a400c858860220017ce797e90c3933513f8b5
Parvinder Singh <psndna88@gmail.com>
2019-02-03 12:50:02 +0530

    sdcardfs: disable debug
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -93,7 +93,7 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 
 		switch (token) {
 		case Opt_debug:
-			*debug = 1;
+			*debug = 0;
 			break;
 		case Opt_fsuid:
 			if (match_int(&args[0], &option))
@@ -180,7 +180,7 @@ int parse_options_remount(struct super_block *sb, char *options, int silent,
 
 		switch (token) {
 		case Opt_debug:
-			debug = 1;
+			debug = 0;
 			break;
 		case Opt_gid:
 			if (match_int(&args[0], &option))
