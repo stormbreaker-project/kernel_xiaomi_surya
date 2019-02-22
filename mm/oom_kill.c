79027bf8a794c99fbac9393779b2f9b3cd861503
freak07 <michalechner92@googlemail.com>
2019-02-22 07:45:58 +0100

    oom_reaper: enable reap mem on sigkill and disable dump on oom
    (cherry picked from commit ca14e43ff0dc6fc893e770685f70f518ac076902)
    (cherry picked from commit 72acf5905141d21ac5a49e35b006af4ee0e16c1f)
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -52,8 +52,8 @@
 
 int sysctl_panic_on_oom;
 int sysctl_oom_kill_allocating_task;
-int sysctl_oom_dump_tasks = 1;
-int sysctl_reap_mem_on_sigkill;
+int sysctl_oom_dump_tasks;
+int sysctl_reap_mem_on_sigkill = 1;
 
 DEFINE_MUTEX(oom_lock);
 /* Serializes oom_score_adj and oom_score_adj_min updates */
