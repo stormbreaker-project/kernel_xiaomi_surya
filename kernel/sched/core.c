ffd0a820bf57533e5fc957ffe2650dbbf715e248
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-05-02 16:45:22 +0530

    sched/walt: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I3af4fccd6db844729863f0422a910e2906c8ff9a
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6297,7 +6297,6 @@ int sched_cpu_activate(unsigned int cpu)
 	rq_unlock_irqrestore(rq, &rf);
 
 	update_max_interval();
-	walt_update_min_max_capacity();
 
 	return 0;
 }
@@ -6337,7 +6336,6 @@ int sched_cpu_deactivate(unsigned int cpu)
 		return ret;
 	}
 	sched_domains_numa_masks_clear(cpu);
-	walt_update_min_max_capacity();
 	return 0;
 }
 
