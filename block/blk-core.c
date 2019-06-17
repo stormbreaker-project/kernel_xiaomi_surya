f4c8c59a37e6046b1d287c8f15e6817c5ae5b684
Tyler Nijmeh <tylernij@gmail.com>
2019-06-16 19:03:04 -0700

    block: core/mq: Use interruptible waits
    Allow these tasks to be preempted in order to reduce latency.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -871,7 +871,7 @@ int blk_queue_enter(struct request_queue *q, unsigned int op)
 		 */
 		smp_rmb();
 
-		wait_event(q->mq_freeze_wq,
+		wait_event_interruptible(q->mq_freeze_wq,
 			   (!atomic_read(&q->mq_freeze_depth) &&
 			   ((op & REQ_PREEMPT) ||
 			    !blk_queue_preempt_only(q))) ||
