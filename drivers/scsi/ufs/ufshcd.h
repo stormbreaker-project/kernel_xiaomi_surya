3e65d35463aed21d199e640dc93429b64130ca66
Martin Liu <liumartin@google.com>
2019-07-22 14:04:05 +0800

    scsi: ufs: Use async operation for hibern8 operation
    When enabling/disabling hibern8, we will call flush_work
    to wait for the task finished. Using async operation
    to avoid long waiting time.
    
    Bug: 138085490
    Test: Boot, cat /sys/kernel/debug/ufshcd0/show_hba and
    check hibern8_exit_cnt
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: I427cd8e40cbc6674a145142babbd92a1062dc511
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -500,6 +500,7 @@ enum ufshcd_hibern8_on_idle_state {
  * @delay_attr: sysfs attribute to control delay_attr
  * @enable_attr: sysfs attribute to enable/disable hibern8 on idle
  * @is_enabled: Indicates the current status of hibern8
+ * @enable_mutex: protect sys node race from multithread access
  */
 struct ufs_hibern8_on_idle {
 	struct delayed_work enter_work;
@@ -511,6 +512,7 @@ struct ufs_hibern8_on_idle {
 	struct device_attribute delay_attr;
 	struct device_attribute enable_attr;
 	bool is_enabled;
+	struct mutex enable_mutex;
 };
 
 /**
@@ -952,6 +954,7 @@ struct ufs_hba {
 	struct work_struct eh_work;
 	struct work_struct eeh_work;
 	struct work_struct rls_work;
+	struct work_struct hibern8_on_idle_enable_work;
 
 	/* HBA Errors */
 	u32 errors;
