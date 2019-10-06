aa36b911243fc6b14df0538b02ba6f335c62f333
Panchajanya1999 <panchajanya@azure-dev.live>
2019-10-06 14:54:39 +0530

    exec: Add power@2.0 and IOP to tampering blacklist
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -156,8 +156,10 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	return !strcmp(comm, "init") || !strcmp(comm, "NodeLooperThrea") ||
 	       !strcmp(comm, "power@1.2-servi") ||
 	       !strcmp(comm, "power@1.3-servi") ||
+	       !strcmp(comm, "power@2.0-servi") ||
 	       !strcmp(comm, "perf@1.0-servic") ||
 	       !strcmp(comm, "perf@2.0-servic") ||
+	       !strcmp(comm, "iop@") ||
 	       !strcmp(comm, "init.qcom.post_") ||
 	       !strcmp(comm, "init.ginkgo");
 }
