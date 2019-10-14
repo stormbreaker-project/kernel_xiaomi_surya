223a273d6c6ffdb5dda0db10adbfc5fe4fcea6ce
Zachariah Kennedy <zkennedy87@gmail.com>
2019-10-14 14:02:22 +0000

    sched/fair: honor sync only if CPU is about to goto idle
    sync is causing excessive latencies during binder replies as its causing
    migration of important tasks to busy CPU. Incase the CPU has a lot of
    tasks running, prevent sync from happening
    
    Based on: https://android.googlesource.com/kernel/msm.git/+/3944217bbbc8f4344a31e5837e926cec630690cc
    
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8131,6 +8131,7 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 	u64 start_t = 0;
 	int next_cpu = -1, backup_cpu = -1;
 	int boosted = (schedtune_task_boost(p) > 0 || per_task_boost(p) > 0);
+	bool about_to_idle = (cpu_rq(cpu)->nr_running < 2);
 
 	fbt_env.fastpath = 0;
 
@@ -8140,7 +8141,7 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 	if (need_idle)
 		sync = 0;
 
-	if (sysctl_sched_sync_hint_enable && sync &&
+	if (sysctl_sched_sync_hint_enable && sync && about_to_idle &&
 				bias_to_waker_cpu(p, cpu, rtg_target)) {
 		target_cpu = cpu;
 		fbt_env.fastpath = SYNC_WAKEUP;
