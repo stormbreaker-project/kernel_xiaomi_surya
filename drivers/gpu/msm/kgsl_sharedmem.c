33c69cfaa396a2456b77947ff1a1e656d420cf2d
Jordan Crouse <jcrouse@codeaurora.org>
2019-10-14 10:41:07 -0600

    [BACKPORT]msm: kgsl: Add an option to always enable I/O coherency Add a Kconfig option to allow the driver to mark all memory buffers as I/O coherent by default if the target supports I/O coherency.
    This is a config option for now because I/O coherency has traditionally
    been a little fickle and it represents a paradigm shift to enable it
    universally. Eventually the goal is to leave it on by default and invert
    the polarity of this option.
    
    In conjunction we can use this option to manage kernel targets that do not
    not support fine grained cache operations. Because it is impossible to stop
    the user from creating and using cached surfaces the driver has
    to either support I/O coherency by default OR have access to fine grained
    cache operations. We can take advantage of the new Kconfig option to
    make some compile time decisions and compile out the cache code if it isn't
    supported.
    
    This isn't 100% foolproof though. If your target doesn't support I/O
    coherency and there are no cache operations available you are out of luck,
    so as it stands this precludes cached operations on a3xx and a5xx with this
    kernel. We will still allow cached surfaces but they will never be
    coherent. If this becomes a problem we'll need to figure out a way to
    disallow cached surfaces on those targets.
    
    Change-Id: Ic0dedbad7d923b54f64f34cd68a59c3522ca5ee9
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -29,6 +29,15 @@
 #include "kgsl_mmu.h"
 
 /*
+ * For now, we either need the low level cache operations or
+ * QCOM_KGSL_IOCOHERENCY_DEFAULT enabled because we can't stop userspace
+ * from expecting to enable cached surfaces and have them work
+ */
+#if !defined(dmac_flush_range) && !IS_ENABLED(CONFIG_QCOM_KGSL_IOCOHERENCY_DEFAULT)
+#error "KGSL needs either dmac_flush_range or CONFIG_QCOM_KGSL_IOCOHERENCY_DEFAULT enabled"
+#endif
+
+/*
  * The user can set this from debugfs to force failed memory allocations to
  * fail without trying OOM first.  This is a debug setting useful for
  * stress applications that want to test failure cases without pushing the
@@ -683,7 +692,8 @@ static inline unsigned int _fixup_cache_range_op(unsigned int op)
 }
 #endif
 
-static inline void _cache_op(unsigned int op,
+#ifdef dmac_flush_range
+static void _cache_op(unsigned int op,
 			const void *start, const void *end)
 {
 	/*
@@ -703,8 +713,8 @@ static inline void _cache_op(unsigned int op,
 	}
 }
 
-static int kgsl_do_cache_op(struct page *page, void *addr,
-		uint64_t offset, uint64_t size, unsigned int op)
+static void kgsl_do_cache_op(struct page *page, void *addr, u64 offset,
+		u64 size, unsigned int op)
 {
 	if (page != NULL) {
 		unsigned long pfn = page_to_pfn(page) + offset / PAGE_SIZE;
@@ -733,15 +743,21 @@ static int kgsl_do_cache_op(struct page *page, void *addr,
 				offset = 0;
 			} while (size);
 
-			return 0;
+			return;
 		}
 
 		addr = page_address(page);
 	}
 
 	_cache_op(op, addr + offset, addr + offset + (size_t) size);
-	return 0;
 }
+#else
+
+static void kgsl_do_cache_op(struct page *page, void *addr, u64 offset,
+		u64 size, unsigned int op)
+{
+}
+#endif
 
 int kgsl_cache_range_op(struct kgsl_memdesc *memdesc, uint64_t offset,
 		uint64_t size, unsigned int op)
@@ -750,7 +766,9 @@ int kgsl_cache_range_op(struct kgsl_memdesc *memdesc, uint64_t offset,
 	struct sg_table *sgt = NULL;
 	struct scatterlist *sg;
 	unsigned int i, pos = 0;
-	int ret = 0;
+
+	if (memdesc->flags & KGSL_MEMFLAGS_IOCOHERENT)
+		return 0;
 
 	if (size == 0 || size > UINT_MAX)
 		return -EINVAL;
@@ -769,8 +787,8 @@ int kgsl_cache_range_op(struct kgsl_memdesc *memdesc, uint64_t offset,
 		if (addr + ((size_t) offset + (size_t) size) < addr)
 			return -ERANGE;
 
-		ret = kgsl_do_cache_op(NULL, addr, offset, size, op);
-		return ret;
+		kgsl_do_cache_op(NULL, addr, offset, size, op);
+		return 0;
 	}
 
 	/*
@@ -781,7 +799,7 @@ int kgsl_cache_range_op(struct kgsl_memdesc *memdesc, uint64_t offset,
 		sgt = memdesc->sgt;
 	else {
 		if (memdesc->pages == NULL)
-			return ret;
+			return 0;
 
 		sgt = kgsl_alloc_sgt_from_pages(memdesc);
 		if (IS_ERR(sgt))
@@ -798,7 +816,7 @@ int kgsl_cache_range_op(struct kgsl_memdesc *memdesc, uint64_t offset,
 		sg_offset = offset > pos ? offset - pos : 0;
 		sg_left = (sg->length - sg_offset > size) ? size :
 					sg->length - sg_offset;
-		ret = kgsl_do_cache_op(sg_page(sg), NULL, sg_offset,
+		kgsl_do_cache_op(sg_page(sg), NULL, sg_offset,
 							sg_left, op);
 		size -= sg_left;
 		if (size == 0)
@@ -809,7 +827,7 @@ int kgsl_cache_range_op(struct kgsl_memdesc *memdesc, uint64_t offset,
 	if (memdesc->sgt == NULL)
 		kgsl_free_sgt(sgt);
 
-	return ret;
+	return 0;
 }
 EXPORT_SYMBOL(kgsl_cache_range_op);
 
@@ -829,8 +847,13 @@ void kgsl_memdesc_init(struct kgsl_device *device,
 		flags &= ~((uint64_t) KGSL_MEMFLAGS_USE_CPU_MAP);
 
 	/* Disable IO coherence if it is not supported on the chip */
-	if (!MMU_FEATURE(mmu, KGSL_MMU_IO_COHERENT))
+	if (!MMU_FEATURE(mmu, KGSL_MMU_IO_COHERENT)) {
 		flags &= ~((uint64_t) KGSL_MEMFLAGS_IOCOHERENT);
+
+		WARN_ONCE(IS_ENABLED(CONFIG_QCOM_KGSL_IOCOHERENCY_DEFAULT),
+			"I/O coherency is not supported on this target\n");
+	} else if (IS_ENABLED(CONFIG_QCOM_KGSL_IOCOHERENCY_DEFAULT))
+		flags |= KGSL_MEMFLAGS_IOCOHERENT;
 
 	if (MMU_FEATURE(mmu, KGSL_MMU_NEED_GUARD_PAGE))
 		memdesc->priv |= KGSL_MEMDESC_GUARD_PAGE;
