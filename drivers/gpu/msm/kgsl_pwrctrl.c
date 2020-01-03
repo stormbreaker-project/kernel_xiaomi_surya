7bf42cc51415a269ca5e1d339715af3b758ec8b0
Yaroslav Furman <yaro330@gmail.com>
2020-01-04 01:00:12 +0300

    drivers: fix a few debug related build errors
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -478,11 +478,6 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 	kgsl_clk_set_rate(device, pwr->active_pwrlevel);
 	_isense_clk_set_rate(pwr, pwr->active_pwrlevel);
 
-	trace_kgsl_pwrlevel(device,
-			pwr->active_pwrlevel, pwrlevel->gpu_freq,
-			pwr->previous_pwrlevel,
-			pwr->pwrlevels[old_level].gpu_freq);
-
 	/*
 	 * Some targets do not support the bandwidth requirement of
 	 * GPU at TURBO, for such targets we need to set GPU-BIMC
