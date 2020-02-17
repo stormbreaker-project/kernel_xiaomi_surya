46674ed8935989200deb5e90ac6102bb300420ac
Pavankumar Kondeti <pkondeti@codeaurora.org>
2020-02-17 11:35:03 +0530

    sched: walt: Improve the scheduler
    This change is for general scheduler improvements.
    
    Change-Id: I4623a59eca688209110c347d388df2a2839a6287
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -254,7 +254,7 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 	} else {
 		if (use_pelt())
 			sg_policy->work_in_progress = true;
-		sched_irq_work_queue(&sg_policy->irq_work);
+		irq_work_queue(&sg_policy->irq_work);
 	}
 }
 
