46323ff9929a9ca12c9f9e8c434003b51429afe3
Yaroslav Furman <yaro330@gmail.com>
2020-04-13 13:27:57 +0300

    kernel: stune_assist: clarify logger a bit
    We are not actually setting WALT specific values, no need in printing them.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -761,15 +761,16 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		struct st_data tgt = st_targets[i];
 
 		if (!strcmp(css->cgroup->kn->name, tgt.name)) {
-			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d colocate=%d no_override=%d\n",
-				tgt.name, tgt.boost, tgt.prefer_idle,
-				tgt.colocate, tgt.no_override);
-
 			boost_write(css, NULL, tgt.boost);
 			prefer_idle_write(css, NULL, tgt.prefer_idle);
-#ifdef CONFIG_SCHED_WALT
+#ifndef CONFIG_SCHED_WALT
+			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d\n",
+				tgt.name, tgt.boost, tgt.prefer_idle);
+#else
 			sched_colocate_write(css, NULL, tgt.colocate);
 			sched_boost_override_write(css, NULL, tgt.no_override);
+			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d colocate=%d no_override=%d\n",
+				tgt.name, tgt.boost, tgt.prefer_idle, tgt.colocate, tgt.no_override);
 #endif
 		}
 	}
