5a8b2f2b9fd284ac9c44e6e74c3267846b8ffbde
Jayaprakash <jmadiset@codeaurora.org>
2020-04-28 23:53:03 +0530

    drm/msm/sde: Modify QoS vote for IRQ latency during idle cases
    Add changes to modify the CPU Qos Vote for irq latency
    during idle usecases.
    
    Change-Id: I07c0ed097c33f755ab4eadab4807063bc688bc24
    Signed-off-by: Jayaprakash <jmadiset@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_encoder.c
+++ drivers/gpu/drm/msm/sde/sde_encoder.c
@@ -2343,6 +2343,13 @@ static int sde_encoder_resource_control(struct drm_encoder *drm_enc,
 
 	switch (sw_event) {
 	case SDE_ENC_RC_EVENT_KICKOFF:
+	{
+		struct msm_drm_private *priv;
+		struct sde_kms *sde_kms;
+
+		priv = drm_enc->dev->dev_private;
+		sde_kms = to_sde_kms(priv->kms);
+
 		/* cancel delayed off work, if any */
 		if (kthread_cancel_delayed_work_sync(
 				&sde_enc->delayed_off_work))
@@ -2371,6 +2378,7 @@ static int sde_encoder_resource_control(struct drm_encoder *drm_enc,
 
 		if (is_vid_mode && sde_enc->rc_state == SDE_ENC_RC_STATE_IDLE) {
 			_sde_encoder_irq_control(drm_enc, true);
+			sde_kms_update_pm_qos_irq_request(sde_kms, true, false);
 		} else {
 			/* enable all the clks and resources */
 			ret = _sde_encoder_resource_control_helper(drm_enc,
@@ -2395,7 +2403,7 @@ static int sde_encoder_resource_control(struct drm_encoder *drm_enc,
 
 		mutex_unlock(&sde_enc->rc_lock);
 		break;
-
+	}
 	case SDE_ENC_RC_EVENT_FRAME_DONE:
 		if (!sde_enc->crtc) {
 			SDE_ERROR("invalid crtc, sw_event:%u\n", sw_event);
@@ -2637,6 +2645,13 @@ static int sde_encoder_resource_control(struct drm_encoder *drm_enc,
 		break;
 
 	case SDE_ENC_RC_EVENT_ENTER_IDLE:
+	{
+		struct msm_drm_private *priv;
+		struct sde_kms *sde_kms;
+
+		priv = drm_enc->dev->dev_private;
+		sde_kms = to_sde_kms(priv->kms);
+
 		mutex_lock(&sde_enc->rc_lock);
 
 		if (sde_enc->rc_state != SDE_ENC_RC_STATE_ON) {
@@ -2664,6 +2679,7 @@ static int sde_encoder_resource_control(struct drm_encoder *drm_enc,
 
 		if (is_vid_mode) {
 			_sde_encoder_irq_control(drm_enc, false);
+			sde_kms_update_pm_qos_irq_request(sde_kms, false, false);
 		} else {
 			/* disable all the clks and resources */
 			_sde_encoder_resource_control_rsc_update(drm_enc,
@@ -2677,6 +2693,7 @@ static int sde_encoder_resource_control(struct drm_encoder *drm_enc,
 
 		mutex_unlock(&sde_enc->rc_lock);
 		break;
+	}
 	case SDE_ENC_RC_EVENT_EARLY_WAKEUP:
 		if (!sde_enc->crtc ||
 			sde_enc->crtc->index >= ARRAY_SIZE(priv->disp_thread)) {
