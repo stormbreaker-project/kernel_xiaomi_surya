95e08f53953da9d91c325a944299340bca026c61
Dyneteve <dyneteve@pixelexperience.org>
2020-06-02 17:03:52 +0200

    power: smb5: Drop the batt_psy checks and use 100mA to reset input
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -8642,8 +8642,7 @@ static void bms_update_work(struct work_struct *work)
 
 	smblib_suspend_on_debug_battery(chg);
 
-	if (chg->batt_psy)
-		power_supply_changed(chg->batt_psy);
+	power_supply_changed(chg->batt_psy);
 }
 
 static void pl_update_work(struct work_struct *work)
