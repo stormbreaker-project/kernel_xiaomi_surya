05adfdc171035e56c294d535915e31fc9c70cbef
Jesse Chan <jc@linux.com>
2020-07-22 08:40:37 +0800

    Revert "ALSA: compress: use mutex in drain"
     * Upstream 4.19 has 7e89ae71856cda9733c917d815b5ecd16c36cccf, f263c156a0046bbe626ae7e5da7147565ce96d65 merged but not this one.
     Align with 4.19 and remove mutex locks to fix compress offload playback.
    
     * This reverts commit d5bdafc4ae0d46c120b9bed6e9c3cb42c3affa98.
    
    Change-Id: I1a74941810a6db714963034b559108386bbd8bb4
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- sound/core/compress_offload.c
+++ sound/core/compress_offload.c
@@ -777,39 +777,29 @@ int snd_compr_stop_error(struct snd_compr_stream *stream,
 }
 EXPORT_SYMBOL_GPL(snd_compr_stop_error);
 
-/* this fn is called without lock being held and we change stream states here
- * so while using the stream state auquire the lock but relase before invoking
- * DSP as the call will possibly take a while
- */
 static int snd_compr_drain(struct snd_compr_stream *stream)
 {
 	int retval;
 
-	mutex_lock(&stream->device->lock);
 	switch (stream->runtime->state) {
 	case SNDRV_PCM_STATE_OPEN:
 	case SNDRV_PCM_STATE_SETUP:
 	case SNDRV_PCM_STATE_PREPARED:
 	case SNDRV_PCM_STATE_PAUSED:
-		retval = -EPERM;
-		goto ret;
+		return -EPERM;
 	case SNDRV_PCM_STATE_XRUN:
-		retval = -EPIPE;
-		goto ret;
+		return -EPIPE;
 	default:
 		break;
 	}
 	mutex_unlock(&stream->device->lock);
 	retval = stream->ops->trigger(stream, SND_COMPR_TRIGGER_DRAIN);
-	mutex_lock(&stream->device->lock);
 	if (!retval) {
 		stream->runtime->state = SNDRV_PCM_STATE_DRAINING;
 		wake_up(&stream->runtime->sleep);
-		goto ret;
+		return retval;
 	}
 
-ret:
-	mutex_unlock(&stream->device->lock);
 	return retval;
 }
 
@@ -849,11 +839,9 @@ static int snd_compr_partial_drain(struct snd_compr_stream *stream)
 	case SNDRV_PCM_STATE_SETUP:
 	case SNDRV_PCM_STATE_PREPARED:
 	case SNDRV_PCM_STATE_PAUSED:
-		retval = -EPERM;
-		goto ret;
+		return -EPERM;
 	case SNDRV_PCM_STATE_XRUN:
-		retval = -EPIPE;
-		goto ret;
+		return -EPIPE;
 	default:
 		break;
 	}
@@ -873,8 +861,6 @@ static int snd_compr_partial_drain(struct snd_compr_stream *stream)
 	stream->next_track = false;
 	return retval;
 
-ret:
-	mutex_unlock(&stream->device->lock);
 	return retval;
 }
 
