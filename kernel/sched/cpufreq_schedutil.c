723e45ad036b508f1e049edfbd03abb7727c0253
Yaroslav Furman <yaro330@gmail.com>
2020-07-27 10:50:15 +0300

    schedutil: revert 3ba6681f7d95a61c28fd78b9f1b103ca994af6ab
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -174,7 +174,7 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 
 		policy->cur = next_freq;
 	} else {
-		if (likely(use_pelt()))
+		if (use_pelt())
 			sg_policy->work_in_progress = true;
 		sched_irq_work_queue(&sg_policy->irq_work);
 	}
@@ -232,7 +232,7 @@ static void sugov_get_util(unsigned long *util, unsigned long *max, int cpu,
 
 	*util = boosted_cpu_util(cpu, &loadcpu->walt_load);
 
-	if (likely(use_pelt())) {
+	if (use_pelt()) {
 		sched_avg_update(rq);
 		delta = time - rq->age_stamp;
 		if (unlikely(delta < 0))
@@ -456,7 +456,7 @@ static void sugov_work(struct kthread_work *work)
 				CPUFREQ_RELATION_L);
 	mutex_unlock(&sg_policy->work_lock);
 
-	if (likely(use_pelt()))
+	if (use_pelt())
 		sg_policy->work_in_progress = false;
 }
 
