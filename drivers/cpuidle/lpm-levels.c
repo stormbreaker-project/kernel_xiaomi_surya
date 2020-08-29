57c201d21c90b9492281fa97a2585ef56d62ee44
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-08-29 19:50:26 +0530

    cpuidle: lpm-levels: Add back bias_hyst parameter
     * this was removed in 86383fe64e25, but qcom perf hal
       needs it and spams logcat if it doesn't exist
    
    Change-Id: I273423f5e121fe3fa8f4cf6a01fb908ffad16f5a
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -93,6 +93,8 @@ struct lpm_cluster *lpm_root_node;
 static bool lpm_prediction = true;
 module_param_named(lpm_prediction, lpm_prediction, bool, 0664);
 
+static uint32_t bias_hyst;
+module_param_named(bias_hyst, bias_hyst, uint, 0664);
 static bool lpm_ipi_prediction = true;
 module_param_named(lpm_ipi_prediction, lpm_ipi_prediction, bool, 0664);
 
