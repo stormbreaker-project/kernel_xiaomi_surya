5bdbe3fdc362e6a8b23fdfa7a448be40754f88e6
celtare21 <celtare21@gmail.com>
2020-09-09 07:45:01 +0000

    userland: Add support for user-tunable configs and adblocker
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- init/userland_worker.c
+++ init/userland_worker.c
@@ -17,41 +17,18 @@
 #include "../security/selinux/include/security.h"
 #include "../security/selinux/include/avc_ss_reset.h"
 
-#define MAX_CHAR 128
+#define LEN(arr) ((int) (sizeof (arr) / sizeof (arr)[0]))
 #define INITIAL_SIZE 4
+#define MAX_CHAR 128
 #define DELAY 500
 
+static const char* path_to_files[] = { "/sdcard/Artemis/adblocker.txt" };
+
+struct values {
+	bool adblocker;
+};
+
 static struct delayed_work userland_work;
-
-static int use_userspace(char** argv)
-{
-	static char* envp[] = {
-		"SHELL=/bin/sh",
-		"HOME=/",
-		"USER=shell",
-		"TERM=xterm-256color",
-		"PATH=/product/bin:/apex/com.android.runtime/bin:/apex/com.android.art/bin:/system_ext/bin:/system/bin:/system/xbin:/odm/bin:/vendor/bin:/vendor/xbin",
-		"DISPLAY=:0",
-		NULL
-	};
-
-	pr_info("Calling userspace!");
-
-	return call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
-}
-
-static inline void set_selinux(int value)
-{
-	pr_info("Setting selinux state: %d", value);
-
-	enforcing_set(extern_state, value);
-	if (value)
-		avc_ss_reset(extern_state->avc, 0);
-	selnl_notify_setenforce(value);
-	selinux_status_update_setenforce(extern_state, value);
-	if (!value)
-		call_lsm_notifier(LSM_POLICY_CHANGE, NULL);
-}
 
 static void free_memory(char** argv, int size)
 {
@@ -77,12 +54,185 @@ static char** alloc_memory(int size)
 		argv[i] = kmalloc(MAX_CHAR * sizeof(char), GFP_KERNEL);
 		if (!argv[i]) {
 			pr_err("Couldn't allocate memory!");
-			free_memory(argv, i);
+			kfree(argv);
 			return NULL;
 		}
 	}
 
 	return argv;
+}
+
+static int use_userspace(char** argv)
+{
+	static char* envp[] = {
+		"SHELL=/bin/sh",
+		"HOME=/",
+		"USER=shell",
+		"TERM=xterm-256color",
+		"PATH=/product/bin:/apex/com.android.runtime/bin:/apex/com.android.art/bin:/system_ext/bin:/system/bin:/system/xbin:/odm/bin:/vendor/bin:/vendor/xbin",
+		"DISPLAY=:0",
+		NULL
+	};
+
+	return call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
+}
+
+static struct file *file_open(const char *path, int flags, umode_t rights)
+{
+	struct file *filp;
+	mm_segment_t oldfs;
+
+	oldfs = get_fs();
+	set_fs(get_ds());
+
+	filp = filp_open(path, flags, rights);
+
+	if (!S_ISREG(file_inode(filp)->i_mode) || filp->f_pos < 0) {
+		filp_close(filp, NULL);
+		set_fs(oldfs);
+		return NULL;
+	}
+
+	set_fs(oldfs);
+
+	if (IS_ERR(filp))
+		return NULL;
+
+	return filp;
+}
+
+static struct values *alloc_and_populate(void)
+{
+	struct file* __file = NULL;
+	struct values* tweaks;
+	unsigned char buf[MAX_CHAR];
+	int size, ret, i;
+	long number_value;
+	loff_t pos = 0;
+
+	tweaks = kmalloc(sizeof(struct values), GFP_KERNEL);
+	if (!tweaks) {
+		pr_err("Couldn't allocate memory!");
+		return NULL;
+	}
+
+	tweaks->adblocker = 0;
+
+	size = LEN(path_to_files);
+	for (i = 0; i < size; i++) {
+		if (path_to_files[i] == NULL)
+			continue;
+
+		__file = file_open(path_to_files[i], O_RDONLY | O_LARGEFILE, 0);
+		if (__file == NULL || IS_ERR_OR_NULL(__file->f_path.dentry)) {
+			pr_err("Couldn't open file %s", path_to_files[i]);
+			continue;
+		}
+
+		memset(buf, 0, MAX_CHAR);
+
+		mdelay(10);
+		ret = kernel_read(__file, buf, MAX_CHAR, &pos);
+		mdelay(10);
+		filp_close(__file, NULL);
+
+		if (ret < 0) {
+			pr_err("Couldn't read buffer!");
+			continue;
+		}
+
+		pr_info("Parsed file %s with value %s", path_to_files[i], buf);
+
+		if (kstrtol(buf, 10, &number_value))
+			continue;
+
+		if (strstr(path_to_files[i], "adblocker")) {
+			tweaks->adblocker = !!number_value;
+			pr_info("Adblocker value: %d", tweaks->adblocker);
+		}
+	}
+
+	return tweaks;
+}
+
+static void create_dirs(void)
+{
+	char** argv;
+	int ret;
+
+	argv = alloc_memory(INITIAL_SIZE - 1);
+	if (!argv) {
+		pr_err("Couldn't allocate memory!");
+		return;
+	}
+
+	strcpy(argv[0], "/system/bin/mkdir");
+	strcpy(argv[1], "/sdcard/Artemis");
+	argv[2] = NULL;
+
+	ret = use_userspace(argv);
+	if (!ret)
+		pr_info("Artemis folder doesn't exist! Creating.");
+	else
+		pr_info("Artemis folder already created");
+
+	strcpy(argv[0], "/system/bin/ls");
+	strcpy(argv[1], "/sdcard/Artemis/adblocker.txt");
+	argv[2] = NULL;
+
+	ret = use_userspace(argv);
+	if (ret) {
+		strcpy(argv[0], "/system/bin/touch");
+		strcpy(argv[1], "/sdcard/Artemis/adblocker.txt");
+		argv[2] = NULL;
+
+		ret = use_userspace(argv);
+		if (!ret) {
+			pr_info("Adblocker file created!");
+
+			free_memory(argv, INITIAL_SIZE - 1);
+
+			argv = alloc_memory(INITIAL_SIZE);
+			if (!argv) {
+				pr_err("Couldn't allocate memory!");
+				return;
+			}
+
+			strcpy(argv[0], "/system/bin/sh");
+			strcpy(argv[1], "-c");
+			strcpy(argv[2], "/system/bin/printf 0 > /sdcard/Artemis/adblocker.txt");
+			argv[3] = NULL;
+
+			ret = use_userspace(argv);
+			if (!ret)
+				pr_info("Printf called succesfully! Adblocker file created!");
+			else
+				pr_err("Couldn't printf to file! %d", ret);
+
+			free_memory(argv, INITIAL_SIZE);
+		} else {
+			pr_err("Couldn't create adblocker file!");
+		}
+	} else {
+		pr_info("Adblocker file exists!");
+	}
+
+	// Wait for RCU grace period to end for the files to sync
+	rcu_barrier();
+	msleep(100);
+}
+
+static inline void set_selinux(int value)
+{
+	pr_info("Setting selinux state: %d", value);
+
+	enforcing_set(extern_state, value);
+	if (value)
+		avc_ss_reset(extern_state->avc, 0);
+	selnl_notify_setenforce(value);
+	selinux_status_update_setenforce(extern_state, value);
+	if (!value)
+		call_lsm_notifier(LSM_POLICY_CHANGE, NULL);
 }
 
 static void encrypted_work(void)
@@ -134,6 +284,7 @@ static void encrypted_work(void)
 
 static void decrypted_work(void)
 {
+	struct values* tweaks;
 	char** argv;
 	int ret;
 
@@ -147,7 +298,79 @@ static void decrypted_work(void)
 		pr_info("Waiting for fs decryption!");
 		while (!is_decrypted)
 			msleep(1000);
+		msleep(10000);
 		pr_info("Fs decrypted!");
+	}
+
+	create_dirs();
+	tweaks = alloc_and_populate();
+
+	if (tweaks && tweaks->adblocker) {
+		strcpy(argv[0], "/system/bin/sh");
+		strcpy(argv[1], "-c");
+		strcpy(argv[2], "/system/bin/iptables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to-destination 176.103.130.130");
+		argv[3] = NULL;
+
+		ret = use_userspace(argv);
+		if (!ret)
+			pr_info("Iptables called succesfully!");
+		else
+			pr_err("Couldn't call iptables! %d", ret);
+
+		strcpy(argv[0], "/system/bin/sh");
+		strcpy(argv[1], "-c");
+		strcpy(argv[2], "/system/bin/iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 176.103.130.130");
+		argv[3] = NULL;
+
+		ret = use_userspace(argv);
+		if (!ret)
+			pr_info("Iptables called succesfully!");
+		else
+			pr_err("Couldn't call iptables! %d", ret);
+
+		strcpy(argv[0], "/system/bin/sh");
+		strcpy(argv[1], "-c");
+		strcpy(argv[2], "/system/bin/iptables -t nat -D OUTPUT -p tcp --dport 53 -j DNAT --to-destination 176.103.130.130 || true");
+		argv[3] = NULL;
+
+		ret = use_userspace(argv);
+		if (!ret)
+			pr_info("Iptables called succesfully!");
+		else
+			pr_err("Couldn't call iptables! %d", ret);
+
+		strcpy(argv[0], "/system/bin/sh");
+		strcpy(argv[1], "-c");
+		strcpy(argv[2], "/system/bin/iptables -t nat -D OUTPUT -p udp --dport 53 -j DNAT --to-destination 176.103.130.130 || true");
+		argv[3] = NULL;
+
+		ret = use_userspace(argv);
+		if (!ret)
+			pr_info("Iptables called succesfully!");
+		else
+			pr_err("Couldn't call iptables! %d", ret);
+
+		strcpy(argv[0], "/system/bin/sh");
+		strcpy(argv[1], "-c");
+		strcpy(argv[2], "/system/bin/iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to-destination 176.103.130.130");
+		argv[3] = NULL;
+
+		ret = use_userspace(argv);
+		if (!ret)
+			pr_info("Iptables called succesfully!");
+		else
+			pr_err("Couldn't call iptables! %d", ret);
+
+		strcpy(argv[0], "/system/bin/sh");
+		strcpy(argv[1], "-c");
+		strcpy(argv[2], "/system/bin/iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to-destination 176.103.130.130");
+		argv[3] = NULL;
+
+		ret = use_userspace(argv);
+		if (!ret)
+			pr_info("Iptables called succesfully!");
+		else
+			pr_err("Couldn't call iptables! %d", ret);
 	}
 
 	strcpy(argv[0], "/system/bin/cp");
@@ -168,9 +391,9 @@ static void decrypted_work(void)
 		if (!ret)
 			pr_info("Chmod called succesfully!");
 		else
-			pr_err("Couldn't call chmod! Exiting %s %d", __func__, ret);
+			pr_err("Couldn't call chmod! Exiting %d", ret);
 	} else {
-		pr_err("Couldn't copy file! %s %d", __func__, ret);
+		pr_err("Couldn't copy file! %d", ret);
 	}
 
 	free_memory(argv, INITIAL_SIZE);
