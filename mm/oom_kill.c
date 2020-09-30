06bf7b6dce081c67fe8f0fef8505a3fd13873a93
THEBOSS619 <ehabhani59@gmail.com>
2020-10-01 00:18:26 +0300

    mm/oom_kill: mark kthread as perf critical (big cluster)
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -631,7 +631,8 @@ void wake_oom_reaper(struct task_struct *tsk)
 
 static int __init oom_init(void)
 {
-	oom_reaper_th = kthread_run(oom_reaper, NULL, "oom_reaper");
+	oom_reaper_th = kthread_run_perf_critical(cpu_perf_mask, oom_reaper,
+					NULL, "oom_reaper");
 	if (IS_ERR(oom_reaper_th)) {
 		pr_err("Unable to start OOM reaper %ld. Continuing regardless\n",
 				PTR_ERR(oom_reaper_th));
