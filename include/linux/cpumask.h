d105acbfc42dbca23b8dce2ca811a69ab6a42a4b
Peter Zijlstra <peterz@infradead.org>
2020-10-01 15:54:14 +0200

    sched,rt: Use cpumask_any*_distribute()
    Replace a bunch of cpumask_any*() instances with
    cpumask_any*_distribute(), by injecting this little bit of random in
    cpu selection, we reduce the chance two competing balance operations
    working off the same lowest_mask pick the same CPU.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Reviewed-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    Link: https://lkml.kernel.org/r/20201023102347.190759694@infradead.org
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -198,6 +198,11 @@ static inline int cpumask_any_and_distribute(const struct cpumask *src1p,
 	return cpumask_next_and(-1, src1p, src2p);
 }
 
+static inline int cpumask_any_distribute(const struct cpumask *srcp)
+{
+	return cpumask_first(srcp);
+}
+
 #define for_each_cpu(cpu, mask)			\
 	for ((cpu) = 0; (cpu) < 1; (cpu)++, (void)mask)
 #define for_each_cpu_not(cpu, mask)		\
@@ -251,6 +256,7 @@ int cpumask_any_but(const struct cpumask *mask, unsigned int cpu);
 unsigned int cpumask_local_spread(unsigned int i, int node);
 int cpumask_any_and_distribute(const struct cpumask *src1p,
 			       const struct cpumask *src2p);
+int cpumask_any_distribute(const struct cpumask *srcp);
 
 /**
  * for_each_cpu - iterate over every cpu in a mask
