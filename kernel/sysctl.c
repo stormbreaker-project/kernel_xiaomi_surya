8ef763fcbbe85b07cbe8ffc3715fa125c71bdc87
alk3pInjection <webmaster@raspii.tech>
2020-11-11 01:53:46 +0800

    sysctl: Expose raw swappiness value to userspace
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    (cherry picked from commit b72b3f653ae585a85a9dfec54662f5061ab28cd8)
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1676,6 +1676,13 @@ static struct ctl_table vm_table[] = {
 		.extra2		= &one_hundred,
 	},
 	{
+		.procname	= "swappiness",
+		.data		= &vm_swappiness,
+		.maxlen		= sizeof(vm_swappiness),
+		.mode		= 0444,
+		.proc_handler	= proc_dointvec,
+	},
+	{
 		.procname       = "want_old_faultaround_pte",
 		.data           = &want_old_faultaround_pte,
 		.maxlen         = sizeof(want_old_faultaround_pte),
