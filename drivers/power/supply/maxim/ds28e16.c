4fc79a173f2f4766aabbd65cb1c901c729420155
Yaroslav Furman <yaro330@gmail.com>
2020-11-21 15:16:24 +0300

    drivers: fix a bunch of warnings
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/power/supply/maxim/ds28e16.c
+++ drivers/power/supply/maxim/ds28e16.c
@@ -822,24 +822,20 @@ unsigned char *Challenge, unsigned char *Secret_Seeds, unsigned char *S_Secret)
 	int msg_len = 0;
 	unsigned char flag = DS_FALSE;
 
-	//if (flag_mi_auth_result)
 	if (mi_auth_result == DS_TRUE)
 		return mi_auth_result;
 
-	//if (anon != ANONYMOUS) {
+	if (ds28el16_Read_RomID_retry(mi_romid) != DS_TRUE) {
+		ow_reset();
+		return ERROR_R_ROMID;
+	}
 
-		if (ds28el16_Read_RomID_retry(mi_romid) != DS_TRUE) {
-			ow_reset();
-			return ERROR_R_ROMID;
-		}
-
-		if (ds28el16_get_page_status_retry(status_chip) == DS_TRUE) {
-			MANID[0] = status_chip[4];
-		} else {
-			ow_reset();
-			return ERROR_R_STATUS;
-		}
-	//}
+	if (ds28el16_get_page_status_retry(status_chip) == DS_TRUE) {
+		MANID[0] = status_chip[4];
+	} else {
+		ow_reset();
+		return ERROR_R_STATUS;
+	}
 
 	// DS28E16 calculate its session secret
 	flag = DS28E16_cmd_computeS_Secret_retry(anon,
