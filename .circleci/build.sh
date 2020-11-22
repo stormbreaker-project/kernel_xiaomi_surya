759bfa123863feb1db3fedf5643545cc024b853a
Forenche <prahul2003@gmail.com>
2020-11-22 21:49:53 +0530

    .circleci: initial setup for circle ci
    Signed-off-by: Forenche <prahul2003@gmail.com>

---
+++ .circleci/build.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/stormbreaker-project/kernel_surya -b  new  kernel
+cd kernel
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/Forenche/AnyKernel3 AnyKernel
+echo "Done"
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+TANGGAL=$(date +"%F-%S")
+START=$(date +"%s")
+export CONFIG_PATH=$PWD/arch/arm64/configs/surya-perf_defconfig
+PATH="${PWD}/clang/bin:$PATH"
+export ARCH=arm64
+export KBUILD_BUILD_HOST=circleci
+export KBUILD_BUILD_USER="forenche"
+# sticker plox
+function sticker() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
+        -d sticker="CAADBQADVAADaEQ4KS3kDsr-OWAUFgQ" \
+        -d chat_id=$chat_id
+}
+# Send info plox channel
+function sendinfo() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>• surya-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Poco x3</b> (RMX1851)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
+}
+# Push kernel to channel
+function push() {
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Poco x3 (surya)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+}
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build threw an error(s)"
+    exit 1
+}
+# Compile plox
+function compile() {
+   make O=out ARCH=arm64 surya-perf_defconfig
+       make -j$(nproc --all) O=out \
+                             ARCH=arm64 \
+			     CC=clang \
+			     CROSS_COMPILE=aarch64-linux-gnu- \
+			     CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+   cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+}
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 surya-Stormbreaker-${TANGGAL}.zip *
+    cd .. 
+}
+sticker
+sendinfo
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
