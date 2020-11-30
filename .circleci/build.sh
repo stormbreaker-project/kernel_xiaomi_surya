3db2af2583ae67d6a67bc303fcb58e2587bcb636
Forenche <prahul2003@gmail.com>
2020-11-30 04:45:13 +0000

    circleci: update for alpha branch
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/stormbreaker-project/kernel_xiaomi_surya/ -b  new  kernel
+git clone --depth=1 https://github.com/stormbreaker-project/kernel_xiaomi_surya/ -b  alpha  kernel
 cd kernel
 git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b surya AnyKernel
