43f332f402162ab5980b9e1e3803a989d528d464
Yaroslav Furman <yaro330@gmail.com>
2020-12-08 19:47:22 +0300

    touchscreen: nt36xxx: optimize coordinate reporting and i2c reads/writes
    - Mark IRQ as always awake and perf critical.
    - Add static branch predictor hints.
    - Throw in pm_qos triggers for good measure.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/input/touchscreen/nt36xxx_spi/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx_spi/nt36xxx.c
@@ -315,6 +315,17 @@ static inline int32_t spi_read_write(struct spi_device *client, uint8_t *buf, si
 	return spi_sync(client, &m);
 }
 
+static void nvt_pm_qos(bool enable)
+{
+	if (unlikely(!pm_qos_request_active(&ts->pm_qos_req)))
+		return;
+
+	if (enable)
+		pm_qos_update_request(&ts->pm_qos_req, 100);
+	else
+		pm_qos_update_request(&ts->pm_qos_req, PM_QOS_DEFAULT_VALUE);
+}
+
 /*******************************************************
 Description:
 	Novatek touchscreen spi read function.
@@ -331,6 +342,7 @@ int32_t CTP_SPI_READ(struct spi_device *client, uint8_t *buf, uint16_t len)
 
 	buf[0] = SPI_READ_MASK(buf[0]);
 
+	nvt_pm_qos(true);
 	while (retries < 5) {
 		ret = spi_read_write(client, buf, len, NVTREAD);
 		if (ret == 0)
@@ -344,6 +356,7 @@ int32_t CTP_SPI_READ(struct spi_device *client, uint8_t *buf, uint16_t len)
 	} else {
 		memcpy((buf+1), (ts->rbuf+2), (len-1));
 	}
+	nvt_pm_qos(false);
 
 	mutex_unlock(&ts->xbuf_lock);
 
@@ -366,6 +379,7 @@ int32_t CTP_SPI_WRITE(struct spi_device *client, uint8_t *buf, uint16_t len)
 
 	buf[0] = SPI_WRITE_MASK(buf[0]);
 
+	nvt_pm_qos(true);
 	while (retries < 5) {
 		ret = spi_read_write(client, buf, len, NVTWRITE);
 		if (ret == 0)
@@ -377,6 +391,7 @@ int32_t CTP_SPI_WRITE(struct spi_device *client, uint8_t *buf, uint16_t len)
 		NVT_ERR("error, ret=%d\n", ret);
 		ret = -EIO;
 	}
+	nvt_pm_qos(false);
 
 	mutex_unlock(&ts->xbuf_lock);
 
@@ -1422,7 +1437,7 @@ static irqreturn_t nvt_ts_work_func(int irq, void *data)
 	}
 #endif
 #if WAKEUP_GESTURE
-	if (bTouchIsAwake == 0) {
+	if (unlikely(!bTouchIsAwake)) {
 		//input_id = (uint8_t)(point_data[1] >> 3);
 		nvt_ts_wakeup_gesture_report(input_id, point_data);
 		mutex_unlock(&ts->lock);
@@ -1431,6 +1446,7 @@ static irqreturn_t nvt_ts_work_func(int irq, void *data)
 #endif
 	finger_cnt = 0;
 
+	nvt_pm_qos(true);
 	for (i = 0; i < ts->max_touch_num; i++) {
 		position = 1 + 6 * i;
 		input_id = (uint8_t)(point_data[position + 0] >> 3);
@@ -1528,7 +1544,7 @@ static irqreturn_t nvt_ts_work_func(int irq, void *data)
 	input_sync(ts->input_dev);
 
 XFER_ERROR:
-
+	nvt_pm_qos(false);
 	mutex_unlock(&ts->lock);
 
 	return IRQ_HANDLED;
@@ -2446,7 +2462,9 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 		NVT_LOG("int_trigger_type=%d\n", ts->int_trigger_type);
 		ts->irq_enabled = true;
 		ret = request_threaded_irq(client->irq, NULL, nvt_ts_work_func,
-				ts->int_trigger_type | IRQF_ONESHOT, NVT_SPI_NAME, ts);
+				ts->int_trigger_type |
+				IRQF_ONESHOT | IRQF_NO_SUSPEND |
+				IRQF_PERF_CRITICAL, NVT_SPI_NAME, ts);
 		if (ret != 0) {
 			NVT_ERR("request irq failed. ret=%d\n", ret);
 			goto err_int_request_failed;
@@ -2455,6 +2473,12 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 			NVT_LOG("request irq %d succeed\n", client->irq);
 		}
 	}
+
+	ts->pm_qos_req.type = PM_QOS_REQ_AFFINE_IRQ;
+	ts->pm_qos_req.irq = client->irq;
+
+	pm_qos_add_request(&ts->pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
+			PM_QOS_DEFAULT_VALUE);
 
 #if WAKEUP_GESTURE
 	device_init_wakeup(&ts->input_dev->dev, 1);
@@ -2774,6 +2798,7 @@ static int32_t nvt_ts_remove(struct spi_device *client)
 {
 	NVT_LOG("Removing driver...\n");
 
+	pm_qos_remove_request(&ts->pm_qos_req);
 #if defined(CONFIG_FB)
 	if (ts->workqueue)
 		destroy_workqueue(ts->workqueue);
@@ -3303,6 +3328,7 @@ static int32_t __init nvt_driver_init(void)
 	int32_t ret = 0;
 
 	NVT_LOG("start\n");
+
 #ifdef CHECK_TOUCH_VENDOR
 	//Check TP vendor
 	if (IS_ERR_OR_NULL(saved_command_line)) {
