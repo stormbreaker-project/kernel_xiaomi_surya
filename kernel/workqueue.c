cc751481e1a6e182847b644a9530e7d00535630b
Peter Zijlstra (Intel) <peterz@infradead.org>
2021-01-22 12:04:12 +0530

    BACKPORT: workqueue: Tag bound workers with KTHREAD_IS_PER_CPU
    Mark the per-cpu workqueue workers as KTHREAD_IS_PER_CPU.
    
    Workqueues have unfortunate semantics in that per-cpu workers are not
    default flushed and parked during hotplug, however a subset does
    manual flush on hotplug and hard relies on them for correctness.
    
    Therefore play silly games..
    
    Change-Id: I460c20813c47bfbddbbb1132ca665d73a6f60020
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Tested-by: Valentin Schneider <valentin.schneider@arm.com>
    Link: https://lkml.kernel.org/r/20210121103506.693465814@infradead.org
    [panchajanya1999: ported to k4.14]
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1775,6 +1775,8 @@ static void worker_attach_to_pool(struct worker *worker,
 	 */
 	if (pool->flags & POOL_DISASSOCIATED)
 		worker->flags |= WORKER_UNBOUND;
+	else
+		kthread_set_per_cpu(worker->task, pool->cpu);
 
 	list_add_tail(&worker->node, &pool->workers);
 
@@ -1796,6 +1798,7 @@ static void worker_detach_from_pool(struct worker *worker,
 	struct completion *detach_completion = NULL;
 
 	mutex_lock(&pool->attach_mutex);
+	kthread_set_per_cpu(worker->task, -1);
 	list_del(&worker->node);
 	if (list_empty(&pool->workers))
 		detach_completion = pool->detach_completion;
@@ -4706,8 +4709,10 @@ static void unbind_workers(int cpu)
 		 * before the last CPU down must be on the cpu.  After
 		 * this, they may become diasporas.
 		 */
-		for_each_pool_worker(worker, pool)
+		for_each_pool_worker(worker, pool) {
+			kthread_set_per_cpu(worker->task, -1);
 			worker->flags |= WORKER_UNBOUND;
+		}
 
 		pool->flags |= POOL_DISASSOCIATED;
 
@@ -4762,9 +4767,11 @@ static void rebind_workers(struct worker_pool *pool)
 	 * of all workers first and then clear UNBOUND.  As we're called
 	 * from CPU_ONLINE, the following shouldn't fail.
 	 */
-	for_each_pool_worker(worker, pool)
+	for_each_pool_worker(worker, pool) {
+		kthread_set_per_cpu(worker->task, pool->cpu);
 		WARN_ON_ONCE(set_cpus_allowed_ptr(worker->task,
 						  pool->attrs->cpumask) < 0);
+	}
 
 	spin_lock_irq(&pool->lock);
 
