1bd020e6cf7e07e4e179bcc300547614e6c231f1
Alessio Balsini <balsini@google.com>
2021-01-25 17:02:28 +0000

    FROMLIST: fuse: Definitions and ioctl for passthrough
    Expose the FUSE_PASSTHROUGH interface to user space and declare all the
    basic data structures and functions as the skeleton on top of which the
    FUSE passthrough functionality will be built.
    
    As part of this, introduce the new FUSE passthrough ioctl, which allows
    the FUSE daemon to specify a direct connection between a FUSE file and a
    lower file system file. Such ioctl requires user space to pass the file
    descriptor of one of its opened files through the fuse_passthrough_out
    data structure introduced in this patch. This structure includes extra
    fields for possible future extensions.
    Also, add the passthrough functions for the set-up and tear-down of the
    data structures and locks that will be used both when fuse_conns and
    fuse_files are created/deleted.
    
    Bug: 179164095
    Link: https://lore.kernel.org/lkml/20210125153057.3623715-4-balsini@android.com/
    Signed-off-by: Alessio Balsini <balsini@android.com>
    Change-Id: I732532581348adadda5b5048a9346c2b0868d539
    Signed-off-by: Alessio Balsini <balsini@google.com>
    (cherry picked from commit d02368d67989781a3484cd8dd71e0079d0d1bda2)
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -531,6 +531,7 @@ static int fuse_create_open(struct inode *dir, struct dentry *entry,
 	ff->open_flags = outopen.open_flags;
 	if (args.private_lower_rw_file != NULL)
 		ff->rw_lower_file = args.private_lower_rw_file;
+	fuse_passthrough_setup(fc, ff, &outopen);
 	inode = fuse_iget(dir->i_sb, outentry.nodeid, outentry.generation,
 			  &outentry.attr, entry_attr_timeout(&outentry), 0);
 	if (!inode) {
