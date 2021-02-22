d0fd31c188c9dfd895b71e3d999188836ba376b9
celtare21 <celtare21@gmail.com>
2021-02-22 10:38:59 -0500

    init: userland: Clean redundant stuff
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- init/userland_worker.c
+++ init/userland_worker.c
@@ -277,38 +277,16 @@ static inline int linux_chmod(const char* path, const char* perms)
 	return use_userspace(argv);
 }
 
-static void fix_TEE(void)
-{
-	int ret, retries = 0;
-
-	msleep(SHORT_DELAY * 2);
-
-	pr_info("Fixing TEE");
-
-	do {
-		ret = linux_write("ro.product.model", "Pixel 4a", true);
-		if (ret)
-			msleep(DELAY);
-	} while (ret && retries++ < 10);
-}
-
 static void encrypted_work(void)
 {
 	if (!linux_sh("/system/bin/su"))
 		is_su = true;
-
-	if (!is_su)
-		 fix_TEE();
 
 	linux_write("debug.hwui.renderer", "skiavk", false);
 
 	linux_write("pixel.oslo.allowed_override", "1", false);
 
 	linux_write("persist.vendor.radio.multisim_swtich_support", "true", false);
-
-	linux_write("dalvik.vm.dex2oat-cpu-set", "0,1,2,3,4,5,6", false);
-
-	linux_write("dalvik.vm.dex2oat-threads", "6", false);
 }
 
 static void decrypted_work(void)
@@ -429,10 +407,6 @@ static void decrypted_work(void)
 	kfree(tweaks);
 
 skip:
-	linux_sh("/system/bin/echo UnityMain,libunity.so > /proc/sys/kernel/sched_lib_name");
-
-	linux_sh("/system/bin/echo 255 > /proc/sys/kernel/sched_lib_mask_force");
-
 	linux_sh("/system/bin/stop vendor.input.classifier-1-0");
 }
 
