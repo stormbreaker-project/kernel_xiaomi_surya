afd625ef41cd0af5a68e48acb2164f19a4c31da4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-01 13:16:47 +0530

    msm_performance: Disable boosting when battery saver is on
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -24,6 +24,7 @@
 #include <linux/sysfs.h>
 #include <linux/module.h>
 #include <linux/input.h>
+#include <linux/battery_saver.h>
 #include <linux/kthread.h>
 #include <linux/sched/core_ctl.h>
 
@@ -58,18 +59,23 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	int i, j, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
+	const char *disable = "0:0 6:0";
 	struct cpu_status *i_cpu_stats;
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 
+	if (is_battery_saver_on())
+		cp = disable;
+
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
+
+	cp = is_battery_saver_on() ? disable : buf;
 
 	/* CPU:value pair */
 	if (!(ntokens % 2))
 		return -EINVAL;
 
-	cp = buf;
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
