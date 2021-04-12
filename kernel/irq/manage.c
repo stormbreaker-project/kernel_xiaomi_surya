c7b66b5ff496bd210ced691d148a5c5bcea843cf
Panchajanya1999 <panchajanya@azure-dev.live>
2021-04-12 21:31:57 +0530

    kernel: Add API to affine DRM and KGSL IRQs on certain CPUs
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -33,6 +33,8 @@ struct irq_desc_list {
 static LIST_HEAD(perf_crit_irqs);
 static DEFINE_RAW_SPINLOCK(perf_irqs_lock);
 static int perf_cpu_index = -1;
+static int perf_cpu_drm_index = -1;
+static int perf_cpu_kgsl_index = -1;
 static bool perf_crit_suspended;
 
 #ifdef CONFIG_IRQ_FORCED_THREADING
@@ -1163,6 +1165,12 @@ static void affine_one_perf_thread(struct irqaction *action)
 	if (action->flags & IRQF_PERF_AFFINE)
 		mask = cpu_perf_mask;
 
+	if (action->flags & IRQF_PERF_DRM_AFFINE)
+		mask = cpu_perf_drm_mask;
+
+	if (action->flags & IRQF_PERF_KGSL_AFFINE)
+		mask = cpu_perf_kgsl_mask;
+
 	action->thread->flags |= PF_PERF_CRITICAL;
 	set_cpus_allowed_ptr(action->thread, mask);
 }
@@ -1185,6 +1193,16 @@ static void affine_one_perf_irq(struct irq_desc *desc, unsigned int perf_flag)
 	if (perf_flag & IRQF_PERF_AFFINE) {
 		mask = cpu_perf_mask;
 		mask_index = &perf_cpu_index;
+	}
+
+	if (perf_flag & IRQF_PERF_DRM_AFFINE) {
+		mask = cpu_perf_drm_mask;
+		mask_index = &perf_cpu_drm_index;
+	}
+
+	if (perf_flag & IRQF_PERF_KGSL_AFFINE) {
+		mask = cpu_perf_kgsl_mask;
+		mask_index = &perf_cpu_kgsl_index;
 	}
 
 	if (!cpumask_intersects(mask, cpu_online_mask)) {
@@ -1260,6 +1278,8 @@ void reaffine_perf_irqs(bool from_hotplug)
 	if (!from_hotplug || !perf_crit_suspended) {
 		perf_crit_suspended = false;
 		perf_cpu_index = -1;
+		perf_cpu_drm_index = -1;
+		perf_cpu_kgsl_index = -1;
 		list_for_each_entry(data, &perf_crit_irqs, list) {
 			struct irq_desc *desc = data->desc;
 
@@ -1529,7 +1549,8 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 			irqd_set(&desc->irq_data, IRQD_NO_BALANCING);
 		}
 
-		if (new->flags & IRQF_PERF_AFFINE) {
+		if (new->flags & (IRQF_PERF_AFFINE | 
+				IRQF_PERF_DRM_AFFINE | IRQF_PERF_KGSL_AFFINE)) {
 			affine_one_perf_thread(new);
 			irqd_set(&desc->irq_data, IRQD_PERF_CRITICAL);
 			*old_ptr = new;
