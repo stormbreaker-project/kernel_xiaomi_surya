9fdd9157bd5dc30d32db391a0f68bcaf30e204ee
Jean Xiao <jianxiao@codeaurora.org>
2021-04-20 10:06:02 +0800

    msm:vidc: Fix NULL pointer when DEBUG_FS is gone
    Add extra checks to avoid NULL pointer derefernce when DEBUG_FS
    is disabled.
    
    Change-Id: Ib4f1247828c2b4a01f2098e5a97b522c88fe658e
    Signed-off-by: Jean Xiao <jianxiao@codeaurora.org>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/media/platform/msm/vidc_3x/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc_3x/msm_vidc_debug.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2019, 2021, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -238,7 +238,8 @@ struct dentry *msm_vidc_debugfs_init_core(struct msm_vidc_core *core,
 
 	snprintf(debugfs_name, MAX_DEBUGFS_NAME, "core%d", core->id);
 	dir = debugfs_create_dir(debugfs_name, parent);
-	if (!dir) {
+	if (IS_ERR_OR_NULL(dir)) {
+		dir = NULL;
 		dprintk(VIDC_ERR, "Failed to create debugfs for msm_vidc\n");
 		goto failed_create_dir;
 	}
@@ -457,7 +458,8 @@ struct dentry *msm_vidc_debugfs_init_inst(struct msm_vidc_inst *inst,
 	idata->inst = inst;
 
 	dir = debugfs_create_dir(debugfs_name, parent);
-	if (!dir) {
+	if (IS_ERR_OR_NULL(dir)) {
+		dir = NULL;
 		dprintk(VIDC_ERR, "Failed to create debugfs for msm_vidc\n");
 		goto failed_create_dir;
 	}
