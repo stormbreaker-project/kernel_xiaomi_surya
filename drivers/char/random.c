866cc466e826ccd93c9c25708e49fc79fcf8bc99
Jebaitedneko <Jebaitedneko@gmail.com>
2021-05-19 06:22:10 +0530

    char: random: use sdevice_{read,write} for {u}random fops
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -301,6 +301,10 @@
 #define ENTROPY_SHIFT 3
 #define ENTROPY_BITS(r) ((r)->entropy_count >> ENTROPY_SHIFT)
 
+#ifdef CONFIG_SRANDOM
+#include <../drivers/char/srandom/srandom.h>
+#endif
+
 /*
  * The minimum number of bits of entropy before we wake up a read on
  * /dev/random.  Should be enough to do a significant reseed.
@@ -1848,6 +1852,7 @@ _random_read(int nonblock, char __user *buf, size_t nbytes)
 	}
 }
 
+#ifndef CONFIG_SRANDOM
 static ssize_t
 random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
@@ -1876,6 +1881,7 @@ urandom_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 	trace_urandom_read(8 * nbytes, 0, ENTROPY_BITS(&input_pool));
 	return ret;
 }
+#endif
 
 static unsigned int
 random_poll(struct file *file, poll_table * wait)
@@ -1922,6 +1928,7 @@ write_pool(struct entropy_store *r, const char __user *buffer, size_t count)
 	return 0;
 }
 
+#ifndef CONFIG_SRANDOM
 static ssize_t random_write(struct file *file, const char __user *buffer,
 			    size_t count, loff_t *ppos)
 {
@@ -1933,6 +1940,7 @@ static ssize_t random_write(struct file *file, const char __user *buffer,
 
 	return (ssize_t)count;
 }
+#endif
 
 static long random_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
 {
@@ -1997,8 +2005,13 @@ static int random_fasync(int fd, struct file *filp, int on)
 }
 
 const struct file_operations random_fops = {
+	#ifdef CONFIG_SRANDOM
+	.read  = sdevice_read,
+	.write = sdevice_write,
+	#else
 	.read  = random_read,
 	.write = random_write,
+	#endif
 	.poll  = random_poll,
 	.unlocked_ioctl = random_ioctl,
 	.fasync = random_fasync,
@@ -2006,8 +2019,13 @@ const struct file_operations random_fops = {
 };
 
 const struct file_operations urandom_fops = {
+	#ifdef CONFIG_SRANDOM
+	.read  = sdevice_read,
+	.write = sdevice_write,
+	#else
 	.read  = urandom_read,
 	.write = random_write,
+	#endif
 	.unlocked_ioctl = random_ioctl,
 	.fasync = random_fasync,
 	.llseek = noop_llseek,
@@ -2034,7 +2052,11 @@ SYSCALL_DEFINE3(getrandom, char __user *, buf, size_t, count,
 		if (unlikely(ret))
 			return ret;
 	}
+	#ifdef CONFIG_SRANDOM
+	return sdevice_read(NULL, buf, count, NULL);
+	#else
 	return urandom_read(NULL, buf, count, NULL);
+	#endif
 }
 
 /********************************************************************
