f137fc8bbd3bfeb66f8ef04fd2fe81ab5429229c
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-06-07 13:02:17 +0530

    build.sh: Build Image.gz and copy dtb to AK3
    Also perform shfmt and shellcheck while we are at it
    
    Change-Id: I88f2c967ba339c234321f4f502081ff8a56d18b1
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- build.sh
+++ build.sh
@@ -11,49 +11,51 @@ DEFCONFIG="vendor/surya-perf_defconfig"
 export PATH="$TC_DIR/bin:$PATH"
 
 if ! [ -d "$TC_DIR" ]; then
-echo "Proton clang not found! Cloning to $TC_DIR..."
-if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang $TC_DIR; then
-echo "Cloning failed! Aborting..."
-exit 1
+	echo "Proton clang not found! Cloning to $TC_DIR..."
+	if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang "$TC_DIR"; then
+		echo "Cloning failed! Aborting..."
+		exit 1
+	fi
 fi
-fi
-
-export KBUILD_BUILD_USER=adithya
-export KBUILD_BUILD_HOST=ghostrider_reborn
 
 if [[ $1 = "-r" || $1 = "--regen" ]]; then
-make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-cp out/defconfig arch/arm64/configs/$DEFCONFIG
-exit
+	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	exit
 fi
 
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
-rm -rf out
+	rm -rf out
 fi
 
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz dtbo.img
 
-if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
-echo -e "\nKernel compiled succesfully! Zipping up...\n"
-if ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3 -b surya; then
-echo -e "\nCloning AnyKernel3 repo failed! Aborting..."
-exit 1
-fi
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-cp out/arch/arm64/boot/dtbo.img AnyKernel3
-rm -f *zip
-cd AnyKernel3
-zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
-cd ..
-rm -rf AnyKernel3
-rm -rf out/arch/arm64/boot
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-echo "Zip: $ZIPNAME"
-curl --upload-file $ZIPNAME http://transfer.sh/$ZIPNAME; echo
+kernel="out/arch/arm64/boot/Image.gz"
+dtb="out/arch/arm64/boot/dts/qcom/sdmmagpie.dtb"
+dtbo="out/arch/arm64/boot/dtbo.img"
+
+if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	if ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3 -b surya; then
+		echo -e "\nCloning AnyKernel3 repo failed! Aborting..."
+		exit 1
+	fi
+	cp $kernel $dtbo AnyKernel3
+	cp $dtb AnyKernel3/dtb
+	rm -f *zip
+	cd AnyKernel3 || exit
+	rm -rf out/arch/arm64/boot
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo "Zip: $ZIPNAME"
+	curl --upload-file "$ZIPNAME" http://transfer.sh/"$ZIPNAME"
+	echo
 else
-echo -e "\nCompilation failed!"
+	echo -e "\nCompilation failed!"
 fi
