371ec3dcddb18fc43974eeff3210037182d7519a
Rakool <61968330+Forenche@users.noreply.github.com>
2021-06-08 22:35:42 +0530

    gpu: drm: dsi_panel: Change max rr to 120 for oos

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1500,7 +1500,7 @@ static int dsi_panel_parse_dfps_caps(struct dsi_panel *panel)
 
 	} else {
 		dfps_caps->dfps_list[0] = 60;
-		dfps_caps->dfps_list[1] = 90;
+		dfps_caps->dfps_list[1] = 120;
 	}
 
 	dfps_caps->dfps_support = true;
