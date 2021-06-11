f6b0f342417703ead6ec68b4e762b5f5f9c9082e
Forenche <prahul2003@gmail.com>
2021-06-11 06:42:10 +0000

    drone: Update for new branches
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kramel.sh
+++ kramel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/stormbreaker-project/kernel_xiaomi_surya -b rebase-comm kernel
+git clone --depth=1 https://github.com/stormbreaker-project/kernel_xiaomi_surya -b main kernel
 cd kernel
 git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b surya AnyKernel
