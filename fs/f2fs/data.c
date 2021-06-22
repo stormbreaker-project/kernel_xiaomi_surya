06a7a916777e1893aff69ec17178ab8861367e5c
Jebaitedneko <Jebaitedneko@gmail.com>
2021-06-22 21:47:30 +0530

    fs: fixup compilation after trace disablement
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -531,10 +531,12 @@ static inline void __submit_bio(struct f2fs_sb_info *sbi,
 			set_sbi_flag(sbi, SBI_NEED_CP);
 	}
 submit_io:
+#if 0
 	if (is_read_io(bio_op(bio)))
 		trace_f2fs_submit_read_bio(sbi->sb, type, bio);
 	else
 		trace_f2fs_submit_write_bio(sbi->sb, type, bio);
+#endif
 	submit_bio(bio);
 }
 
@@ -582,10 +584,12 @@ static void __submit_merged_bio(struct f2fs_bio_info *io)
 	__attach_io_flag(fio);
 	bio_set_op_attrs(io->bio, fio->op, fio->op_flags);
 
+#if 0
 	if (is_read_io(fio->op))
 		trace_f2fs_prepare_read_bio(io->sbi->sb, fio->type, io->bio);
 	else
 		trace_f2fs_prepare_write_bio(io->sbi->sb, fio->type, io->bio);
+#endif
 
 	__submit_bio(io->sbi, io->bio, fio->type);
 	io->bio = NULL;
