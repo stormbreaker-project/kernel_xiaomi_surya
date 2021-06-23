b6cfe90ae7b986aa293207be8274edc75fb47eab
Jebaitedneko <Jebaitedneko@gmail.com>
2021-06-23 06:29:17 +0530

    fs: f2fs: backport ufs tw changes from 4.19
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- fs/jbd2/commit.c
+++ fs/jbd2/commit.c
@@ -29,6 +29,10 @@
 #include <linux/bitops.h>
 #include <trace/events/jbd2.h>
 
+#if defined(CONFIG_UFSTW) && defined(CONFIG_UFSFEATURE30)
+#include <linux/ufstw.h>
+#endif
+
 /*
  * IO end handler for temporary buffer_heads handling writes to the journal.
  */
@@ -532,6 +536,10 @@ void jbd2_journal_commit_transaction(journal_t *journal)
 	write_unlock(&journal->j_state_lock);
 
 	jbd_debug(3, "JBD2: commit phase 2a\n");
+
+#if defined(CONFIG_UFSTW) && defined(CONFIG_UFSFEATURE30)
+	bdev_set_turbo_write(journal->j_dev);
+#endif
 
 	/*
 	 * Now start flushing things to disk, in the order they appear
@@ -1132,6 +1140,9 @@ restart_loop:
 	write_unlock(&journal->j_state_lock);
 	wake_up(&journal->j_wait_done_commit);
 
+#if defined(CONFIG_UFSTW) && defined(CONFIG_UFSFEATURE30)
+	bdev_clear_turbo_write(journal->j_dev);
+#endif
 	/*
 	 * Calculate overall stats
 	 */
