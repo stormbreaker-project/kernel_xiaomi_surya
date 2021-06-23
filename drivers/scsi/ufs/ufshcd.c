8310f710834cf03b13b54815c27bf15900adb426
Jebaitedneko <Jebaitedneko@gmail.com>
2021-06-23 07:47:59 +0530

    scsi: ufs: remove redundant Qualcomm PM QoS Calls
    These had to be reintroduced in the tw/hpb backport. But Since https://github.com/Jebaitedneko/android_kernel_xiaomi_vayu/commit/c8f628a92ae91d01cd58d3c74b86c80d22a7a01c and https://github.com/Jebaitedneko/android_kernel_xiaomi_vayu/commit/4726d07d37f0038f51fef2dccc562ea165af5d77, we have a much more sane QoS setup and thus this code becomes deprecated. Remove it to clear up double requests.
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -3933,8 +3933,6 @@ static int ufshcd_queuecommand(struct Scsi_Host *host, struct scsi_cmnd *cmd)
 		hba->lrb[tag].hpb_ctx_id = MAX_HPB_CONTEXT_ID;
 send_orig_cmd:
 #endif
-	/* Vote PM QoS for the request */
-	ufshcd_vops_pm_qos_req_start(hba, cmd->request);
 
 	/* IO svc time latency histogram */
 	if (hba != NULL && cmd->request != NULL) {
@@ -4047,7 +4045,6 @@ out:
 		add_lrbp->cmd = NULL;
 		clear_bit_unlock(add_tag, &hba->lrb_in_use);
 		ufshcd_release_all(hba);
-		ufshcd_vops_pm_qos_req_end(hba, pre_cmd->request, true);
 		ufshcd_complete_lrbp_crypto(hba, pre_cmd, add_lrbp);
 		ufsf_hpb_end_pre_req(&hba->ufsf, pre_cmd->request);
 	}
