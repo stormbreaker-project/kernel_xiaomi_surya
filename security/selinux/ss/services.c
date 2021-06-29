52882d5b4c9e80597347486c0cba415b4a1d27ac
Stephen Smalley <sds@tycho.nsa.gov>
2021-06-29 15:35:09 +0530

    BACKPORT: FROMGIT: [PATCH] selinux: fix handling of uninitialized selinux state in  get_bools/classes
    If security_get_bools/classes are called before the selinux state is
    initialized (i.e. before first policy load), then they should just
    return immediately with no booleans/classes.
    
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -2830,6 +2830,13 @@ int security_get_bools(struct selinux_state *state,
 	struct policydb *policydb;
 	int i, rc;
 
+	if (!state->initialized) {
+		*len = 0;
+		*names = NULL;
+		*values = NULL;
+		return 0;
+	}
+
 	read_lock(&state->ss->policy_rwlock);
 
 	policydb = &state->ss->policydb;
@@ -3162,6 +3169,12 @@ int security_get_classes(struct selinux_state *state,
 {
 	struct policydb *policydb = &state->ss->policydb;
 	int rc;
+
+	if (!state->initialized) {
+		*nclasses = 0;
+		*classes = NULL;
+		return 0;
+	}
 
 	read_lock(&state->ss->policy_rwlock);
 
