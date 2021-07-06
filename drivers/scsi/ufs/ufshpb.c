802ac0e2cc1ca21390ed4476f885b840e43e5f4c
Rakool <61968330+Forenche@users.noreply.github.com>
2021-07-06 22:38:33 +0530

    scsi: ufshpb: Remove PM QOS requests
    Already nuked in commit https://github.com/stormbreaker-project/kernel_xiaomi_surya/commit/62a2c0af88c35cae623c7c116e0903b36572639b

--- drivers/scsi/ufs/ufshpb.c
+++ drivers/scsi/ufs/ufshpb.c
@@ -764,10 +764,7 @@ int ufshpb_prepare_add_lrbp(struct ufsf_feature *ufsf, int add_tag)
 		ufshcd_release(hba, true);
 		goto hold_err;
 	}
-
-	/* Vote PM QoS for the pre_req */
-	ufshcd_vops_pm_qos_req_start(hba, pre_cmd->request);
-
+	
 	/* IO svc time latency histogram */
 	if (hba != NULL && pre_cmd->request != NULL) {
 		if (hba->latency_hist_enabled) {
@@ -793,7 +790,6 @@ int ufshpb_prepare_add_lrbp(struct ufsf_feature *ufsf, int add_tag)
 	return 0;
 
 map_err:
-	ufshcd_vops_pm_qos_req_end(hba, pre_cmd->request, true);
 	ufshcd_release_all(hba);
 hold_err:
 	add_lrbp->cmd = NULL;
