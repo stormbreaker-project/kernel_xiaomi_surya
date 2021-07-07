25483261d6ebca2b733a942d5bd31efdfb68b809
Forenche <prahul2003@gmail.com>
2021-07-07 10:43:32 +0000

    simple_lmk: Nuke the ‘ MSM_DRM_BLANK_LP’ case
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -355,7 +355,6 @@ static int msm_drm_notifier_callback(struct notifier_block *self,
 	blank = evdata->data;
 	switch (*blank) {
 	case MSM_DRM_BLANK_POWERDOWN:
-	case MSM_DRM_BLANK_LP:
 		if (!screen_on)
 			break;
 		screen_on = false;
