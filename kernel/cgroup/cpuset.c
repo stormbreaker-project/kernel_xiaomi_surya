32ed23381170713a385686a9deb8dbc2aa687ed1
John Galt <johngaltfirstrun@gmail.com>
2021-07-12 08:31:07 -0400

    cpuset/uclamp_assist: expand/tune
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2132,8 +2132,10 @@ static void uclamp_set(struct kernfs_open_file *of,
 		{"top-app",    	     	"10", "100", 1, 1},
 		{"foreground", 	     	"0",  "50",  0, 0},
 		{"background", 	     	"20", "100", 0, 0},
-		{"system-background", 	"0",  "40",  0, 0},
+		{"system-background", 	"0",  "30",  0, 0},
 		{"camera-daemon",	"50", "100", 1, 1},
+		{"display",		"50", "100", 1, 1},
+		{"restricted",		"0",  "30",  0, 0},
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
