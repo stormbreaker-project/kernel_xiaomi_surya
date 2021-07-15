ac82025997ab434120c6e4efb6a6065b4ed36f3b
Yaroslav Furman <yaro330@gmail.com>
2021-07-15 13:36:41 +0700

    mm: vmscan: turn 'trying to isolate tail page' into a regular logger
    I am not too sure how to fix this.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1682,7 +1682,8 @@ int isolate_lru_page(struct page *page)
 	int ret = -EBUSY;
 
 	VM_BUG_ON_PAGE(!page_count(page), page);
-	WARN_RATELIMIT(PageTail(page), "trying to isolate tail page");
+	if (PageTail(page))
+		pr_err_ratelimited("trying to isolate tail page");
 
 	if (PageLRU(page)) {
 		struct zone *zone = page_zone(page);
