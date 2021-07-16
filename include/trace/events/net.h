807cd459e57310d9c55e6b19935a42925a91091a
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-07-16 14:27:44 +0700

    trace: net: Fix format specifier warning
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- include/trace/events/net.h
+++ include/trace/events/net.h
@@ -67,7 +67,7 @@ TRACE_EVENT(net_dev_start_xmit,
 		  "vlan_proto=0x%04x vlan_tci=0x%04x protocol=0x%04x\t"
 		  "ip_summed=%d len=%u data_len=%u network_offset=%d\t"
 		  "transport_offset_valid=%d transport_offset=%d tx_flags=%d\t"
-		  "gso_size=%d gso_segs=%d gso_type=%#x UTC: %ld",
+		  "gso_size=%d gso_segs=%d gso_type=%#x UTC: %lld",
 		  __get_str(name), __entry->queue_mapping, __entry->skbaddr,
 		  __entry->vlan_tagged, __entry->vlan_proto, __entry->vlan_tci,
 		  __entry->protocol, __entry->ip_summed, __entry->len,
@@ -97,7 +97,7 @@ TRACE_EVENT(net_receive_skb_exit,
 
 	),
 
-	TP_printk(" skbaddr=%pK UTC = %ld", __entry->skbaddr, __entry->utctime)
+	TP_printk(" skbaddr=%pK UTC = %lld", __entry->skbaddr, __entry->utctime)
 );
 
 TRACE_EVENT(net_dev_xmit,
@@ -125,7 +125,7 @@ TRACE_EVENT(net_dev_xmit,
 		__entry->utctime = ktime_get_tai_ns();
 	),
 
-	TP_printk("dev=%s skbaddr=%pK len=%u rc=%d UTC: %ld",
+	TP_printk("dev=%s skbaddr=%pK len=%u rc=%d UTC: %lld",
 		  __get_str(name), __entry->skbaddr, __entry->len, __entry->rc,
 		  __entry->utctime)
 );
@@ -150,7 +150,7 @@ DECLARE_EVENT_CLASS(net_dev_template,
 		__entry->utctime = ktime_get_tai_ns();
 	),
 
-	TP_printk("dev=%s skbaddr=%pK len=%u UTC: %ld",
+	TP_printk("dev=%s skbaddr=%pK len=%u UTC: %lld",
 		__get_str(name), __entry->skbaddr, __entry->len,
 		__entry->utctime)
 )
@@ -231,7 +231,7 @@ DECLARE_EVENT_CLASS(net_dev_rx_verbose_template,
 		  "vlan_tagged=%d vlan_proto=0x%04x vlan_tci=0x%04x\t"
 		  "protocol=0x%04x ip_summed=%d hash=0x%08x l4_hash=%d\t"
 		  "len=%u data_len=%u truesize=%u mac_header_valid=%d\t"
-		  "mac_header=%d nr_frags=%d gso_size=%d gso_type=%#x UTC: %ld",
+		  "mac_header=%d nr_frags=%d gso_size=%d gso_type=%#x UTC: %lld",
 		  __get_str(name), __entry->napi_id, __entry->queue_mapping,
 		  __entry->skbaddr, __entry->vlan_tagged, __entry->vlan_proto,
 		  __entry->vlan_tci, __entry->protocol, __entry->ip_summed,
