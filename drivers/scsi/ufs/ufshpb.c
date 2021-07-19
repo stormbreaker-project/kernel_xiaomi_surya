acb05b50ac3459553d9c0411237663f0f30b59a0
Forenche <prahul2003@gmail.com>
2021-07-19 06:23:40 +0000

    Revert "scsi: ufshpb: Remove PM QOS requests"
    This reverts commit 802ac0e2cc1ca21390ed4476f885b840e43e5f4c.
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/scsi/ufs/ufshpb.c
+++ drivers/scsi/ufs/ufshpb.c
@@ -764,7 +764,10 @@ int ufshpb_prepare_add_lrbp(struct ufsf_feature *ufsf, int add_tag)
 		ufshcd_release(hba, true);
 		goto hold_err;
 	}
-	
+
+	/* Vote PM QoS for the pre_req */
+	ufshcd_vops_pm_qos_req_start(hba, pre_cmd->request);
+
 	/* IO svc time latency histogram */
 	if (hba != NULL && pre_cmd->request != NULL) {
 		if (hba->latency_hist_enabled) {
@@ -790,6 +793,7 @@ int ufshpb_prepare_add_lrbp(struct ufsf_feature *ufsf, int add_tag)
 	return 0;
 
 map_err:
+	ufshcd_vops_pm_qos_req_end(hba, pre_cmd->request, true);
 	ufshcd_release_all(hba);
 hold_err:
 	add_lrbp->cmd = NULL;
