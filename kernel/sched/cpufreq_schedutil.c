a0493de31d144bd46da37d39b2d9fe9efa0ba5b4
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-07-24 17:14:55 +0700

    schedutil: Apply init protection for configure tunables
    Most custom ROMs on ginkgo device already have tunables
    configured in shedutil, so block init in order to use
    tunable configuration from kernel.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -17,6 +17,7 @@
 #include <linux/slab.h>
 #include <trace/events/power.h>
 #include <linux/sched/sysctl.h>
+#include <linux/binfmts.h>
 #include "sched.h"
 
 #define SUGOV_KTHREAD_PRIORITY	50
@@ -658,6 +659,9 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -677,6 +681,9 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
+
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
@@ -703,6 +710,9 @@ static ssize_t hispeed_load_store(struct gov_attr_set *attr_set,
 {
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtouint(buf, 10, &tunables->hispeed_load))
 		return -EINVAL;
 
@@ -726,6 +736,9 @@ static ssize_t hispeed_freq_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned long hs_util;
 	unsigned long flags;
+
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &val))
 		return -EINVAL;
@@ -754,6 +767,9 @@ static ssize_t pl_store(struct gov_attr_set *attr_set, const char *buf,
 				   size_t count)
 {
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
+
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtobool(buf, &tunables->pl))
 		return -EINVAL;
