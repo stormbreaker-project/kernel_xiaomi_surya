3417f7e9124c6ac83bc3e89ba2fddb6f0da97dbd
Forenche <61968330+Forenche@users.noreply.github.com>
2021-08-02 13:37:50 +0530

    sched/tune: Disable prefer.idle for foreground apps

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -779,7 +779,7 @@ static void write_default_values(struct cgroup_subsys_state *css)
 	static struct st_data st_targets[] = {
 		{ "audio-app",	0, 0, 0, 0 },
 		{ "background",	0, 0, 0, 0 },
-		{ "foreground",	0, 1, 0, 1 },
+		{ "foreground",	0, 0, 0, 1 },
 		{ "rt",		0, 0, 0, 0 },
 		{ "top-app",	0, 0, 0, 1 },
 	};
