43b5b2f8c69983076b51bb8e7f26e68d47291733
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2021-08-06 09:16:08 +0700

    net: core: Increase netdev budget and max_backlog
    This configuration is targeting 10G Network, this should be sufficient
    for most of our usecase
    
    BUG: h/r/net/int/28
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3572,11 +3572,11 @@ EXPORT_SYMBOL(dev_queue_xmit_accel);
  *			Receiver routines
  *************************************************************************/
 
-int netdev_max_backlog __read_mostly = 1000;
+int netdev_max_backlog __read_mostly = 3000;
 EXPORT_SYMBOL(netdev_max_backlog);
 
 int netdev_tstamp_prequeue __read_mostly = 1;
-int netdev_budget __read_mostly = 300;
+int netdev_budget __read_mostly = 600;
 /* Must be at least 2 jiffes to guarantee 1 jiffy timeout */
 unsigned int __read_mostly netdev_budget_usecs = 2 * USEC_PER_SEC / HZ;
 int weight_p __read_mostly = 64;           /* old backlog weight */
