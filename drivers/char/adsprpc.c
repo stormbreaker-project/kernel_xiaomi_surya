2a8b56b407587e300f594020815e445007b08846
Sultan Alsawaf <sultan@kerneltoast.com>
2021-08-12 04:15:56 +0530

    qos: Replace cpumasks with atomic bitwise operations
    This is expensive and unneeded, especially since the qos functions are
    hot code paths. Mose of the cpumask functions use the bitmap API, which
    is also more expensive than just doing some simple operations on a word.
    
    Since we're operating with a CPU count that can fit within a word,
    replace the expensive cpumask operations with plain bitwise operations
    wherever possible to make the pm_qos framework more efficient.
    
    Change-Id: I5921bebba29b9f7afb18faa5c7e3c937b91e6688
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Kailash <kailash.sudhakar@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -3715,7 +3715,7 @@ static int fastrpc_internal_control(struct fastrpc_file *fl,
 		for (i = 0; i < len; i++)
 			cpumask_set_cpu(me->silvercores.coreno[i], &mask);
 		fl->pm_qos_req.type = PM_QOS_REQ_AFFINE_CORES;
-		cpumask_copy(&fl->pm_qos_req.cpus_affine, &mask);
+		atomic_set(&fl->pm_qos_req.cpus_affine, *cpumask_bits(&mask));
 		if (!fl->qos_request) {
 			pm_qos_add_request(&fl->pm_qos_req,
 				PM_QOS_CPU_DMA_LATENCY, latency);
