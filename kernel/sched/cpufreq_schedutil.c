6eba68b1b319541079f0370c7c303655bea361c6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-14 14:39:43 +0530

    schedutil: Force conservative_pl when battery saver is on
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -405,7 +405,7 @@ static void sugov_walt_adjust(struct sugov_cpu *sg_cpu, unsigned long *util,
 		*util = *max;
 
 	if (sg_policy->tunables->pl) {
-		if (conservative_pl())
+		if (conservative_pl() || is_battery_saver_on())
 			pl = mult_frac(pl, TARGET_LOAD, 100);
 		*util = max(*util, pl);
 	}
