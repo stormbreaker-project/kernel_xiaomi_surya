65671a076395cf5e794c365c2e52dfbe63aa66fb
Vinayak Menon <vinmenon@codeaurora.org>
2021-08-17 12:53:54 +0530

    mm: process_reclaim: consider compound pages
    Avoid passing tail pages to isolate_lru_page. In the case
    process reclaim, since only lru pages are considered, this
    is just to avoid a warning from isolate_lru_page.
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1711,7 +1711,7 @@ cont:
 		if (page_mapcount(page) != 1)
 			continue;
 
-		if (isolate_lru_page(page))
+		if (isolate_lru_page(compound_head(page)))
 			continue;
 
 		/* MADV_FREE clears pte dirty bit and then marks the page
