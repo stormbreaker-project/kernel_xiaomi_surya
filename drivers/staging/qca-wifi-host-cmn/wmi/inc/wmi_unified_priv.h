a6bc17d8e80e501e827e53181eb8ee1e2755332c
Park Ju Hyung <qkrwngud825@gmail.com>
2021-08-19 06:38:59 +0530

    qcacld-3.0: Disable debugging bloat as much as possible
    * Also enable 6ghz features
    
    The default defconfig enables a lot of debugging features.
    Disable those by referencing other perf defconfigs.
    
    Thankfully, the new 5.2.022 tree requires much less hacks to disable debugging.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Change-Id: I123a52f249e121a5063442caed92600a41976477
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/staging/qca-wifi-host-cmn/wmi/inc/wmi_unified_priv.h
+++ drivers/staging/qca-wifi-host-cmn/wmi/inc/wmi_unified_priv.h
@@ -110,6 +110,8 @@ struct wmi_ext_dbg_msg {
 };
 #endif /*WMI_EXT_DBG */
 
+#ifdef WMI_INTERFACE_EVENT_LOGGING
+
 #ifndef WMI_EVENT_DEBUG_MAX_ENTRY
 #define WMI_EVENT_DEBUG_MAX_ENTRY (1024)
 #endif
@@ -288,6 +290,28 @@ enum WMI_RECORD_TYPE {
 	WMI_CMD = 1,
 	WMI_EVT = 2,
 };
+
+#else
+
+#define wmi_alert(params...) ((void)0)
+#define wmi_err(params...) ((void)0)
+#define wmi_warn(params...) ((void)0)
+#define wmi_info(params...) ((void)0)
+#define wmi_debug(params...) ((void)0)
+
+#define wmi_nofl_alert(params...) ((void)0)
+#define wmi_nofl_err(params...) ((void)0)
+#define wmi_nofl_warn(params...) ((void)0)
+#define wmi_nofl_info(params...) ((void)0)
+#define wmi_nofl_debug(params...) ((void)0)
+
+#define wmi_alert_rl(params...) ((void)0)
+#define wmi_err_rl(params...) ((void)0)
+#define wmi_warn_rl(params...) ((void)0)
+#define wmi_info_rl(params...) ((void)0)
+#define wmi_debug_rl(params...) ((void)0)
+
+#endif /*WMI_INTERFACE_EVENT_LOGGING */
 
 #ifdef WLAN_OPEN_SOURCE
 struct fwdebug {
