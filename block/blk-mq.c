a4fbc84516164946857e58d5222f97509c569bd2
forenche <prahul2003@gmail.com>
2021-08-21 18:12:54 +0530

    Revert "blk-mq: Do not wake idle CPUs to queue same-origin requests"
    This reverts commit 4ae5b322d7498940db8285574e893082d38da980.
    
    Signed-off-by: forenche <prahul2003@gmail.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -557,8 +557,7 @@ static void __blk_mq_complete_request(struct request *rq)
 	}
 
 	cpu = get_cpu();
-	if (!test_bit(QUEUE_FLAG_SAME_FORCE, &rq->q->queue_flags) ||
-			idle_cpu(ctx->cpu))
+	if (!test_bit(QUEUE_FLAG_SAME_FORCE, &rq->q->queue_flags))
 		shared = cpus_share_cache(cpu, ctx->cpu);
 
 	if (cpu != ctx->cpu && !shared && cpu_online(ctx->cpu)) {
