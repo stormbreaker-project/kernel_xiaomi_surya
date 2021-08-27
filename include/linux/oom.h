98ce33f85ed0cb997f422c90466dd059b08706f3
Jebaitedneko <Jebaitedneko@gmail.com>
2021-08-27 10:59:15 +0530

    include: linux: oom.h: don't stub out find_lock_task_mm
    I had missed adding this crucial piece of code that makes SLMK work properly.
    This fixes https://github.com/Jebaitedneko/android_kernel_xiaomi_vayu/commit/bfee6bf07e1f9aee9cd26a05e262efb2e7810567
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- include/linux/oom.h
+++ include/linux/oom.h
@@ -183,9 +183,24 @@ static inline void oom_killer_enable(void)
 	return;
 }
 
+#include <linux/rcupdate.h>
+#include <linux/sched/task.h>
 static inline struct task_struct *find_lock_task_mm(struct task_struct *p)
 {
-	struct task_struct *t = NULL;
+	struct task_struct *t;
+
+	rcu_read_lock();
+
+	for_each_thread(p, t) {
+		task_lock(t);
+		if (likely(t->mm))
+			goto found;
+		task_unlock(t);
+	}
+	t = NULL;
+found:
+	rcu_read_unlock();
+
 	return t;
 }
 
