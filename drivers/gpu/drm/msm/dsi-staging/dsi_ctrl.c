857b065796ef8d6c72989e7545bbb680eabd0c37
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-09-01 01:58:32 +0700

    Revert "disp: msm: enable event log and disp ramdump without DEBUG_FS"
    This reverts commit 5667f5fe7885a0001262bedc258c0d9d730583bc.
    
    We really don't want to enable debug, dump, and logging
    when DEBUG_FS is disabled.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_ctrl.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_ctrl.c
@@ -1927,6 +1927,7 @@ static struct platform_driver dsi_ctrl_driver = {
 	},
 };
 
+#if defined(CONFIG_DEBUG_FS)
 
 void dsi_ctrl_debug_dump(u32 *entries, u32 size)
 {
@@ -1948,6 +1949,7 @@ void dsi_ctrl_debug_dump(u32 *entries, u32 size)
 	mutex_unlock(&dsi_ctrl_list_lock);
 }
 
+#endif
 /**
  * dsi_ctrl_get() - get a dsi_ctrl handle from an of_node
  * @of_node:    of_node of the DSI controller.
