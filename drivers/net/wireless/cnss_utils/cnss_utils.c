794b8c2dd25ce55638009a4677e6a456de662d95
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-09-06 12:21:08 +0700

    cnss_utils: Don't create debugfs when CONFIG_DEBUG_FS=n
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/net/wireless/cnss_utils/cnss_utils.c
+++ drivers/net/wireless/cnss_utils/cnss_utils.c
@@ -12,7 +12,9 @@
 
 #define pr_fmt(fmt) "cnss_utils: " fmt
 
+#ifdef CONFIG_DEBUG_FS
 #include <linux/debugfs.h>
+#endif
 #include <linux/etherdevice.h>
 #include <linux/bitops.h>
 #include <linux/io.h>
@@ -586,6 +588,7 @@ static const struct file_operations cnss_utils_mac_fops = {
 	.llseek		= seq_lseek,
 };
 
+#ifdef CONFIG_DEBUG_FS
 static int cnss_utils_debugfs_create(struct cnss_utils_priv *priv)
 {
 	int ret = 0;
@@ -604,6 +607,7 @@ static int cnss_utils_debugfs_create(struct cnss_utils_priv *priv)
 out:
 	return ret;
 }
+#endif
 
 static int __init cnss_utils_init(void)
 {
@@ -617,7 +621,9 @@ static int __init cnss_utils_init(void)
 
 	mutex_init(&priv->unsafe_channel_list_lock);
 	spin_lock_init(&priv->dfs_nol_info_lock);
+#ifdef CONFIG_DEBUG_FS
 	cnss_utils_debugfs_create(priv);
+#endif
 	cnss_utils_priv = priv;
 
 	return 0;
