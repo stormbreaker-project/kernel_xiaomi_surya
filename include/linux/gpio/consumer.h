801bdbdb477470fcbc929d1456ce78f8a4077e82
Forenche <prahul2003@gmail.com>
2021-09-15 16:49:14 +0530

    Revert "gpio: Propagate errors from gpiod_set_array_value_complex()"
    This reverts commit 88eecfe8c8a03803fbb09440af2f79503e53529a.
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- include/linux/gpio/consumer.h
+++ include/linux/gpio/consumer.h
@@ -101,7 +101,7 @@ int gpiod_direction_output_raw(struct gpio_desc *desc, int value);
 /* Value get/set from non-sleeping context */
 int gpiod_get_value(const struct gpio_desc *desc);
 void gpiod_set_value(struct gpio_desc *desc, int value);
-int gpiod_set_array_value(unsigned int array_size,
+void gpiod_set_array_value(unsigned int array_size,
 			   struct gpio_desc **desc_array, int *value_array);
 int gpiod_get_raw_value(const struct gpio_desc *desc);
 void gpiod_set_raw_value(struct gpio_desc *desc, int value);
@@ -112,7 +112,7 @@ int gpiod_set_raw_array_value(unsigned int array_size,
 /* Value get/set from sleeping context */
 int gpiod_get_value_cansleep(const struct gpio_desc *desc);
 void gpiod_set_value_cansleep(struct gpio_desc *desc, int value);
-int gpiod_set_array_value_cansleep(unsigned int array_size,
+void gpiod_set_array_value_cansleep(unsigned int array_size,
 				    struct gpio_desc **desc_array,
 				    int *value_array);
 int gpiod_get_raw_value_cansleep(const struct gpio_desc *desc);
@@ -311,13 +311,12 @@ static inline void gpiod_set_value(struct gpio_desc *desc, int value)
 	/* GPIO can never have been requested */
 	WARN_ON(1);
 }
-static inline int gpiod_set_array_value(unsigned int array_size,
+static inline void gpiod_set_array_value(unsigned int array_size,
 					 struct gpio_desc **desc_array,
 					 int *value_array)
 {
 	/* GPIO can never have been requested */
 	WARN_ON(1);
-	return 0;
 }
 static inline int gpiod_get_raw_value(const struct gpio_desc *desc)
 {
@@ -350,13 +349,12 @@ static inline void gpiod_set_value_cansleep(struct gpio_desc *desc, int value)
 	/* GPIO can never have been requested */
 	WARN_ON(1);
 }
-static inline int gpiod_set_array_value_cansleep(unsigned int array_size,
+static inline void gpiod_set_array_value_cansleep(unsigned int array_size,
 					    struct gpio_desc **desc_array,
 					    int *value_array)
 {
 	/* GPIO can never have been requested */
 	WARN_ON(1);
-	return 0;
 }
 static inline int gpiod_get_raw_value_cansleep(const struct gpio_desc *desc)
 {
