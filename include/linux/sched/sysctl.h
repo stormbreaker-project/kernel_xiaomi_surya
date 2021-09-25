15d56f995d2aeb0ef3979307763ca1020740c234
Juhyung Park <qkrwngud825@gmail.com>
2021-09-25 19:49:17 +0900

    BACKPORT: sched: promote nodes out of CONFIG_SCHED_DEBUG
    - This patch was backported from [1].
    
    [1]: https://github.com/arter97/android_kernel_oneplus_sm8350/commit/0ed5b00f732e8ee71e8c10e9df169fbc80ca0819
    
    [cyberknight777: backport to 4.14]
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -100,14 +100,12 @@ extern unsigned int sysctl_numa_balancing_scan_period_max;
 extern unsigned int sysctl_numa_balancing_scan_size;
 
 extern __read_mostly unsigned int sysctl_sched_migration_cost;
-#ifdef CONFIG_SCHED_DEBUG
 extern __read_mostly unsigned int sysctl_sched_nr_migrate;
 extern __read_mostly unsigned int sysctl_sched_time_avg;
 
 int sched_proc_update_handler(struct ctl_table *table, int write,
 		void __user *buffer, size_t *length,
 		loff_t *ppos);
-#endif
 
 extern int sched_boost_handler(struct ctl_table *table, int write,
 			void __user *buffer, size_t *lenp, loff_t *ppos);
