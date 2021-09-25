f8066a613b9bdba4d7a1ce77c83968ef1289d5b8
Forenche <prahul2003@gmail.com>
2021-09-25 21:26:53 +0530

    ARM: dts: sdmmagpie-gpu.dtsi: Clock down 180mhz step
    140Mhz seems to be the lowest possible frequency to boot and work with and is good enough for light loads such as scrolling through applications without any lags. Add a custom voltage level because using the retention level is causing GPU faults
    on few devices and using the LOW SVS voltage level does not make much sense since the clock
    speed itself is so low thus leaving us some room to undervolt it so let's just add a custom voltage level which is slightly
    higher than the retention level[1] and is lower than the LOW SVS level and reduce the bus frequency as 140Mhz shouldn't be much demanding.
    
    1: It was kind of working at first but then the GPU kept stalling and
       kgsl kept spamming of GPU faults so avoid the retention voltage level
    
    NOTE: This still requires testing and is advised not to be merged at the moment until it is fully tested.
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/clk/qcom/gpucc-sdmmagpie.c
+++ drivers/clk/qcom/gpucc-sdmmagpie.c
@@ -39,6 +39,7 @@
 
 enum vdd_gx_levels {
 	VDD_GX_NONE,
+	VDD_GX_CUSTOM_SVS,	/* CUSTOM_SVS */
 	VDD_GX_MIN,		/* MIN SVS */
 	VDD_GX_LOWER,		/* SVS2 */
 	VDD_GX_LOW,		/* SVS */
@@ -52,6 +53,7 @@ enum vdd_gx_levels {
 
 static int vdd_gx_corner[] = {
 	RPMH_REGULATOR_LEVEL_OFF,		/* VDD_GX_NONE */
+	RPMH_REGULATOR_LEVEL_CUSTOM_SVS,	/* VDD_GX_CUSTOM_SVS */
 	RPMH_REGULATOR_LEVEL_MIN_SVS,		/* VDD_GX_MIN */
 	RPMH_REGULATOR_LEVEL_LOW_SVS,		/* VDD_GX_LOWER */
 	RPMH_REGULATOR_LEVEL_SVS,		/* VDD_GX_LOW */
@@ -277,7 +279,7 @@ static struct clk_rcg2 gpu_cc_gmu_clk_src = {
 };
 
 static const struct freq_tbl ftbl_gpu_cc_gx_gfx3d_clk_src[] = {
-	F(180000000, P_CRC_DIV, 1, 0, 0),
+	F(140000000, P_CRC_DIV, 1, 0, 0),
 	F(267000000, P_CRC_DIV, 1, 0, 0),
 	F(355000000, P_CRC_DIV, 1, 0, 0),
 	F(430000000, P_CRC_DIV, 1, 0, 0),
@@ -308,7 +310,7 @@ static struct clk_rcg2 gpu_cc_gx_gfx3d_clk_src = {
 		.vdd_class = &vdd_gx,
 		.num_rate_max = VDD_GX_NUM,
 		.rate_max = (unsigned long[VDD_GX_NUM]) {
-			[VDD_GX_MIN] = 180000000,
+			[VDD_GX_CUSTOM_SVS] = 140000000,
 			[VDD_GX_MIN] = 267000000,
 			[VDD_GX_MIN] = 355000000,
 			[VDD_GX_LOW] = 430000000,
