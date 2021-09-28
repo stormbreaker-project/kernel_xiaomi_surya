4b395a981bdf61a10b9d7ea26a3174b99b0e3dc5
Sultanxda <sultanxda@gmail.com>
2021-09-28 10:21:55 +0530

    gf_spi: Set fingerprintd priority to max when screen is off
    Give fingerprintd the highest priority (MIN_NICE) when the screen is off to
    speed up fingerprint processing, and then reset its priority back to normal
    when the screen is on.
    
    There are now 2 fingerprint related processes:
     - android.hardware.biometrics.fingerprint@2.1-service -> comm:fingerprint@2.1
     - com.qualcomm.qti.biometrics.fingerprint.service -> comm:erprint.service
    
    For the match to work properly, use strstr() with "erprint".
    
    Change-Id: Ice4a384f99ae4201b2e5e942b0c8cc16c1190f52
    Co-authored-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Forenche <prahul2003@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/input/fingerprint/gf3208/gf_spi.c
+++ drivers/input/fingerprint/gf3208/gf_spi.c
@@ -662,6 +662,18 @@ static const struct file_operations proc_file_ops = {
 	.release = single_release,
 };
 
+static void set_fingerprintd_nice(int nice)
+{
+	struct task_struct *p;
+
+	read_lock(&tasklist_lock);
+	for_each_process(p) {
+		if (strstr(p->comm, "erprint"))
+			set_user_nice(p, nice);
+	}
+	read_unlock(&tasklist_lock);
+}
+
 static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 		unsigned long val, void *data)
 {
@@ -678,6 +690,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 	if (evdata && evdata->data && val == MSM_DRM_EVENT_BLANK && gf_dev) {
 		blank = evdata->data;
 		if (gf_dev->device_available == 1 && *blank == MSM_DRM_BLANK_UNBLANK) {
+				set_fingerprintd_nice(0);
 				gf_dev->fb_black = 0;
 #if defined(GF_NETLINK_ENABLE)
 				msg[0] = GF_NET_EVENT_FB_UNBLACK;
@@ -691,6 +704,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 	}else if(evdata && evdata->data && val == MSM_DRM_EARLY_EVENT_BLANK && gf_dev){
 		blank = evdata->data;
 			if (gf_dev->device_available == 1 && *blank == MSM_DRM_BLANK_POWERDOWN) {
+				set_fingerprintd_nice(MIN_NICE);
 				gf_dev->fb_black = 1;
 				gf_dev->wait_finger_down = true;
 #if defined(GF_NETLINK_ENABLE)
