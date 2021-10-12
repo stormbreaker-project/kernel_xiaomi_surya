8d766234bc5707aedfb75f3a3e7fa202f9d6eb8f
Danny Lin <danny@kdrag0n.dev>
2021-10-12 13:26:03 +0200

    clk: qcom: mdss: Omit support for unused PLLs
    This kernel won't be used on devices with 10nm, 14nm, or 28nm PLLs nor
    will it be used with the 7nm DisplayPort PLL since our only display is
    connected via DSI.
    
    Don't compile support for PLLs we won't use.
    
    xNombre: sdmmagpie uses 10nm, account for that
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Change-Id: I2d0010979fecf7514f7bff341c6f34ae5e3e0e98
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/clk/qcom/mdss/mdss-pll.c
+++ drivers/clk/qcom/mdss/mdss-pll.c
@@ -130,31 +130,7 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 		pll_res->pll_interface_type = MDSS_DSI_PLL_10NM;
 	if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_10nm"))
 		pll_res->pll_interface_type = MDSS_DP_PLL_10NM;
-	else if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_7nm"))
-		pll_res->pll_interface_type = MDSS_DP_PLL_7NM;
-	else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_7nm"))
-		pll_res->pll_interface_type = MDSS_DSI_PLL_7NM;
-	else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_7nm_v2"))
-		pll_res->pll_interface_type = MDSS_DSI_PLL_7NM_V2;
-	else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_12nm"))
-		pll_res->pll_interface_type = MDSS_DSI_PLL_12NM;
-	else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_28lpm"))
-		pll_res->pll_interface_type = MDSS_DSI_PLL_28LPM;
-	else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_14nm"))
-		pll_res->pll_interface_type = MDSS_DSI_PLL_14NM;
-	else if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_14nm"))
-		pll_res->pll_interface_type = MDSS_DP_PLL_14NM;
-	else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll_28lpm"))
-		pll_res->pll_interface_type = MDSS_HDMI_PLL_28LPM;
-	else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_sdm660")) {
-		pll_res->pll_interface_type = MDSS_DSI_PLL_14NM;
-		pll_res->target_id = MDSS_PLL_TARGET_SDM660;
-		pll_res->revision = 2;
-	} else if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_sdm660")) {
-		pll_res->pll_interface_type = MDSS_DP_PLL_14NM;
-		pll_res->target_id = MDSS_PLL_TARGET_SDM660;
-		pll_res->revision = 2;
-	} else
+	else
 		goto err;
 
 	return rc;
@@ -181,28 +157,6 @@ static int mdss_pll_clock_register(struct platform_device *pdev,
 		break;
 	case MDSS_DP_PLL_10NM:
 		rc = dp_pll_clock_register_10nm(pdev, pll_res);
-		break;
-	case MDSS_DSI_PLL_7NM:
-	case MDSS_DSI_PLL_7NM_V2:
-		rc = dsi_pll_clock_register_7nm(pdev, pll_res);
-		break;
-	case MDSS_DP_PLL_7NM:
-		rc = dp_pll_clock_register_7nm(pdev, pll_res);
-		break;
-	case MDSS_DSI_PLL_28LPM:
-		rc = dsi_pll_clock_register_28lpm(pdev, pll_res);
-		break;
-	case MDSS_DSI_PLL_14NM:
-		rc = dsi_pll_clock_register_14nm(pdev, pll_res);
-		break;
-	case MDSS_DP_PLL_14NM:
-		rc = dp_pll_clock_register_14nm(pdev, pll_res);
-		break;
-	case MDSS_HDMI_PLL_28LPM:
-		rc = hdmi_pll_clock_register_28lpm(pdev, pll_res);
-		break;
-	case MDSS_DSI_PLL_12NM:
-		rc = dsi_pll_clock_register_12nm(pdev, pll_res);
 		break;
 	case MDSS_UNKNOWN_PLL:
 	default:
@@ -434,16 +388,6 @@ static int mdss_pll_remove(struct platform_device *pdev)
 static const struct of_device_id mdss_pll_dt_match[] = {
 	{.compatible = "qcom,mdss_dsi_pll_10nm"},
 	{.compatible = "qcom,mdss_dp_pll_10nm"},
-	{.compatible = "qcom,mdss_dsi_pll_7nm"},
-	{.compatible = "qcom,mdss_dsi_pll_7nm_v2"},
-	{.compatible = "qcom,mdss_dp_pll_7nm"},
-	{.compatible = "qcom,mdss_dsi_pll_28lpm"},
-	{.compatible = "qcom,mdss_dsi_pll_14nm"},
-	{.compatible = "qcom,mdss_dp_pll_14nm"},
-	{.compatible = "qcom,mdss_hdmi_pll_28lpm"},
-	{.compatible = "qcom,mdss_dsi_pll_sdm660"},
-	{.compatible = "qcom,mdss_dsi_pll_12nm"},
-	{.compatible = "qcom,mdss_dp_pll_sdm660"},
 	{}
 };
 
