af7dbedd1f1a39919f6708ea8689b17c7634b721
Bart Van Assche <bvanassche@acm.org>
2021-10-17 22:28:54 +0200

    loop: Select I/O scheduler 'none' from inside add_disk()
    We noticed that the user interface of Android devices becomes very slow
    under memory pressure. This is because Android uses the zram driver on top
    of the loop driver for swapping, because under memory pressure the swap
    code alternates reads and writes quickly, because mq-deadline is the
    default scheduler for loop devices and because mq-deadline delays writes by
    five seconds for such a workload with default settings. Fix this by making
    the kernel select I/O scheduler 'none' from inside add_disk() for loop
    devices. This default can be overridden at any time from user space,
    e.g. via a udev rule. This approach has an advantage compared to changing
    the I/O scheduler from userspace from 'mq-deadline' into 'none', namely
    that synchronize_rcu() does not get called.
    
    Additionally, this patch reduces the Android boot time on my test setup
    with 0.5 seconds compared to configuring the loop I/O scheduler from user
    space.
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Bug: 194450129
    (cherry picked from commit 2112f5c1330a671fa852051d85cb9eadc05d7eb7
                        git://git.kernel.dk/linux-block/ for-5.15/block)
    Change-Id: I6f9579b4cd2cb22fcb5c858d4f292f1870336fdd
    Signed-off-by: Bart Van Assche <bvanassche@google.com>
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- block/elevator.c
+++ block/elevator.c
@@ -194,6 +194,9 @@ int elevator_init(struct request_queue *q, char *name)
 	struct elevator_type *e = NULL;
 	int err;
 
+	if (q->tag_set && q->tag_set->flags & BLK_MQ_F_NO_SCHED_BY_DEFAULT)
+		return 0;
+
 	/*
 	 * q->sysfs_lock must be held to provide mutual exclusion between
 	 * elevator_switch() and here.
