67b5f52edf66450785b52292ffb3bffc5feec81a
Sultan Alsawaf <sultan@kerneltoast.com>
2021-10-19 06:44:56 +0700

    qos: Change cpus_affine to not be atomic
    There isn't a need for cpus_affine to be atomic, and reading/writing to
    it outside of the global pm_qos lock is racy anyway. As such, we can
    simply turn it into a primitive integer type.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5072,7 +5072,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 		struct pm_qos_request *qos = &device->pwrctrl.l2pc_cpus_qos;
 
 		qos->type = PM_QOS_REQ_AFFINE_CORES;
-		atomic_set(&qos->cpus_affine, device->pwrctrl.l2pc_cpus_mask);
+		qos->cpus_affine = device->pwrctrl.l2pc_cpus_mask;
 		pm_qos_add_request(&device->pwrctrl.l2pc_cpus_qos,
 				PM_QOS_CPU_DMA_LATENCY,
 				PM_QOS_DEFAULT_VALUE);
