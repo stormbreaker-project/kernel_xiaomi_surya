ef225eca5afeadb2834b92eaef3bd264fe5c40e8
Park Ju Hyung <qkrwngud825@gmail.com>
2021-10-22 17:09:25 +0800

    sysctl: promote several nodes out of CONFIG_SCHED_DEBUG
    These are used in Android.
    Promote these to disable CONFIG_SCHED_DEBUG.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -79,11 +79,11 @@ extern unsigned int sysctl_numa_balancing_scan_size;
 extern __read_mostly unsigned int sysctl_sched_migration_cost;
 extern __read_mostly unsigned int sysctl_sched_nr_migrate;
 extern __read_mostly unsigned int sysctl_sched_time_avg;
+#endif
 
 int sched_proc_update_handler(struct ctl_table *table, int write,
 		void __user *buffer, size_t *length,
 		loff_t *ppos);
-#endif
 
 extern int sched_boost_handler(struct ctl_table *table, int write,
 			void __user *buffer, size_t *lenp, loff_t *ppos);
