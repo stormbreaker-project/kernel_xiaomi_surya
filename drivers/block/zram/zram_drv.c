2bd9e81162abc6f9fe5d12eb4cb570e3e108b680
Ming Lei <ming.lei@redhat.com>
2021-10-25 10:54:25 +0800

    BACKPORT: zram: avoid race between zram_remove and disksize_store
    After resetting device in zram_remove(), disksize_store still may come and
    allocate resources again before deleting gendisk, fix the race by resetting
    zram after del_gendisk() returns. At that time, disksize_store can't come
    any more.
    
    Reported-by: Luis Chamberlain <mcgrof@kernel.org>
    Reviewed-by: Luis Chamberlain <mcgrof@kernel.org>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Link: https://lore.kernel.org/r/20211025025426.2815424-4-ming.lei@redhat.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    [cyberknight777: backport to 4.14]
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2124,6 +2124,14 @@ static int zram_remove(struct zram *zram)
 	del_gendisk(zram->disk);
 	/* del_gendisk drains pending reset_store */
 	WARN_ON_ONCE(claimed && zram->claim);
+
+	/*
+	 * disksize_store() may be called in between zram_reset_device()
+	 * and del_gendisk(), so run the last reset to avoid leaking
+	 * anything allocated with disksize_store()
+	 */
+	zram_reset_device(zram);
+
 	blk_cleanup_disk(zram->disk);
 	zram0 = NULL;
 	kfree(zram);
