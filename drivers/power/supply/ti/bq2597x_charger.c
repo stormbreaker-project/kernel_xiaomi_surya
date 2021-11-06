a2ba9a56dd8229f9bdbfb8d748e3d530ff76ce84
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-06 14:12:12 +0530

    power: supply: Silence some retarded logspam
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/power/supply/ti/bq2597x_charger.c
+++ drivers/power/supply/ti/bq2597x_charger.c
@@ -2441,7 +2441,7 @@ static int bq2597x_suspend(struct device *dev)
 	mutex_unlock(&bq->irq_complete);
 	bq2597x_enable_adc(bq, false);
 	cancel_delayed_work_sync(&bq->monitor_work);
-	bq_err("Suspend successfully!");
+	bq_info("Suspend successfully!");
 
 	return 0;
 }
@@ -2477,7 +2477,7 @@ static int bq2597x_resume(struct device *dev)
 
 	bq2597x_enable_adc(bq, true);
 	power_supply_changed(bq->fc2_psy);
-	bq_err("Resume successfully!");
+	bq_info("Resume successfully!");
 
 	return 0;
 }
