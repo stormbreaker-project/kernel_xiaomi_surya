bcfa45a1b26b639e2e958bd7baddf8be3248c973
Johannes Weiner <hannes@cmpxchg.org>
2021-11-13 00:31:59 +0100

    BACKPORT: psi: Pressure states are unlikely
    Move the unlikely branches out of line. This eliminates undesirable
    jumps during wakeup and sleeps for workloads that aren't under any
    sort of resource pressure.
    
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lkml.kernel.org/r/20210303034659.91735-4-zhouchengming@bytedance.com
    
    xNombre: Partial backport as cgroup-specific changes are not picked
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sched/psi.c
+++ kernel/sched/psi.c
@@ -226,15 +226,15 @@ static bool test_state(unsigned int *tasks, enum psi_states state)
 {
 	switch (state) {
 	case PSI_IO_SOME:
-		return tasks[NR_IOWAIT];
+		return unlikely(tasks[NR_IOWAIT]);
 	case PSI_IO_FULL:
-		return tasks[NR_IOWAIT] && !tasks[NR_RUNNING];
+		return unlikely(tasks[NR_IOWAIT] && !tasks[NR_RUNNING]);
 	case PSI_MEM_SOME:
-		return tasks[NR_MEMSTALL];
+		return unlikely(tasks[NR_MEMSTALL]);
 	case PSI_MEM_FULL:
-		return tasks[NR_MEMSTALL] && !tasks[NR_RUNNING];
+		return unlikely(tasks[NR_MEMSTALL] && !tasks[NR_RUNNING]);
 	case PSI_CPU_SOME:
-		return tasks[NR_RUNNING] > tasks[NR_ONCPU];
+		return unlikely(tasks[NR_RUNNING] > tasks[NR_ONCPU]);
 	case PSI_NONIDLE:
 		return tasks[NR_IOWAIT] || tasks[NR_MEMSTALL] ||
 			tasks[NR_RUNNING];
