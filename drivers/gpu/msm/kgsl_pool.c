5c6e3015b50acd3effc1b0b00f9a011bf61ffbf9
Jordan Crouse <jcrouse@codeaurora.org>
2021-11-25 18:48:54 +0100

    BACKPORT: msm: kgsl: Use DMA APIs for memory pool cache maintenance
    After allocating and zeroing pages from system memory or the pool we need
    to ensure that the cache is synchronized so that it doesn't cause problems
    down the road.  Use dma_sync_sg_for_device to make sure the allocated pages
    are clean. This isn't the best way to handle this but we haven't yet come
    up with a better way and this does the job.
    
    xNombre: Adopt to 4.14, should help with expensive zero-memsetting page vs
    using dedicated clear_page.
    
    Inspired by: https://github.com/Jebaitedneko/android_kernel_xiaomi_vayu/commit/2b085f4448dd0f19ff3630c9d2ffcc5d8401ee08
    Change-Id: Ic0dedbade48b700015bec172cf9b64e436364b4a
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    (cherry picked from commit 271e0be3866e87a7551548ca4ab5aba4e71a1ed4)
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Foreche <prahul2003@gmail.com>

--- drivers/gpu/msm/kgsl_pool.c
+++ drivers/gpu/msm/kgsl_pool.c
@@ -63,28 +63,42 @@ _kgsl_get_pool_from_order(unsigned int order)
 	return NULL;
 }
 
+static void kgsl_pool_sync_for_device(struct device *dev, struct page *page,
+		size_t size)
+{
+	struct scatterlist sg;
+
+	/* The caller may choose not to specify a device on purpose */
+	if (!dev)
+		return;
+
+	sg_init_table(&sg, 1);
+	sg_set_page(&sg, page, size, 0);
+	sg_dma_address(&sg) = page_to_phys(page);
+
+	dma_sync_sg_for_device(dev, &sg, 1, DMA_BIDIRECTIONAL);
+}
+
 /* Map the page into kernel and zero it out */
 static void
-_kgsl_pool_zero_page(struct page *p, unsigned int pool_order)
+_kgsl_pool_zero_page(struct page *p, unsigned int pool_order,
+		struct device *dev)
 {
 	int i;
 
 	for (i = 0; i < (1 << pool_order); i++) {
 		struct page *page = nth_page(p, i);
-		void *addr = kmap_atomic(page);
 
-		memset(addr, 0, PAGE_SIZE);
-		dmac_flush_range(addr, addr + PAGE_SIZE);
-		kunmap_atomic(addr);
+		clear_highpage(page);
 	}
+
+	kgsl_pool_sync_for_device(dev, p, PAGE_SIZE << pool_order);
 }
 
 /* Add a page to specified pool */
 static void
 _kgsl_pool_add_page(struct kgsl_page_pool *pool, struct page *p)
 {
-	_kgsl_pool_zero_page(p, pool->pool_order);
-
 	llist_add((struct llist_node *)&p->lru, &pool->page_list);
 	atomic_inc(&pool->page_count);
 	mod_node_page_state(page_pgdat(p), NR_KERNEL_MISC_RECLAIMABLE,
@@ -307,7 +321,7 @@ static int kgsl_pool_get_retry_order(unsigned int order)
  * Return total page count on success and negative value on failure
  */
 int kgsl_pool_alloc_page(int *page_size, struct page **pages,
-			unsigned int pages_len, unsigned int *align)
+			unsigned int pages_len, unsigned int *align, struct device *dev)
 {
 	int j;
 	int pcount = 0;
@@ -335,7 +349,6 @@ int kgsl_pool_alloc_page(int *page_size, struct page **pages,
 			} else
 				return -ENOMEM;
 		}
-		_kgsl_pool_zero_page(page, order);
 		goto done;
 	}
 
@@ -355,7 +368,6 @@ int kgsl_pool_alloc_page(int *page_size, struct page **pages,
 			page = alloc_pages(gfp_mask, order);
 			if (page == NULL)
 				return -ENOMEM;
-			_kgsl_pool_zero_page(page, order);
 			goto done;
 		}
 	}
@@ -385,11 +397,11 @@ int kgsl_pool_alloc_page(int *page_size, struct page **pages,
 			} else
 				return -ENOMEM;
 		}
-
-		_kgsl_pool_zero_page(page, order);
 	}
 
 done:
+	_kgsl_pool_zero_page(page, order, dev);
+
 	for (j = 0; j < (*page_size >> PAGE_SHIFT); j++) {
 		p = nth_page(page, j);
 		pages[pcount] = p;
