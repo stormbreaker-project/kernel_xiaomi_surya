110cccfa4e5e297de3543148994416bc53f13ad1
Forenche <prahul2003@gmail.com>
2021-11-26 10:51:52 +0530

    Revert "locking/qspinlock: Use smp_store_release() in queued_spin_unlock()"
    This reverts commit eb2a7a57aae42c1b5e4e0e87290a52e93c542868.
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- include/asm-generic/qspinlock.h
+++ include/asm-generic/qspinlock.h
@@ -100,7 +100,7 @@ static __always_inline void queued_spin_unlock(struct qspinlock *lock)
 	/*
 	 * unlock() needs release semantics:
 	 */
-	smp_store_release(&lock->locked, 0);
+	(void)atomic_sub_return_release(_Q_LOCKED_VAL, &lock->val);
 }
 #endif
 
