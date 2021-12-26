ff8f85bfd1fed7c9121dce75cda11f28630f096a
Forenche <prahul2003@gmail.com>
2021-12-26 13:54:59 +0530

    Revert "sched: use sysctl to control energy aware feature"
    This reverts commit 96c3c72c4200aeeb4da252acc76277c7c7a66377.
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -134,11 +134,6 @@ static unsigned int sched_nr_latency = 8;
 unsigned int sysctl_sched_child_runs_first __read_mostly;
 
 /*
- * To enable/disable energy aware feature.
- */
-unsigned int __read_mostly sysctl_sched_energy_aware = 1;
-
-/*
  * SCHED_OTHER wake-up granularity.
  *
  * This option delays the preemption effects of decoupled workloads
@@ -5791,11 +5786,6 @@ unsigned long capacity_curr_of(int cpu)
 	unsigned long scale_freq = arch_scale_freq_capacity(NULL, cpu);
 
 	return cap_scale(max_cap, scale_freq);
-}
-
-inline bool energy_aware(void)
-{
-	return sysctl_sched_energy_aware;
 }
 
 /*
