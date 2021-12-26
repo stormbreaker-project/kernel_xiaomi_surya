506e1db474ed4bb9bf19b099b6258ac2c76601fb
Forenche <prahul2003@gmail.com>
2021-12-26 20:10:42 +0530

    Revert "soc: qcom: smp2p: Don't check for NULL before ipc_log_string()"
    This reverts commit c636e5cd83415af19f2bef5857692495acc2e101.
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/soc/qcom/smp2p.c
+++ drivers/soc/qcom/smp2p.c
@@ -173,7 +173,10 @@ struct qcom_smp2p {
 static void *ilc;
 #define SMP2P_LOG_PAGE_CNT 2
 #define SMP2P_INFO(x, ...)	\
-	ipc_log_string(ilc, "[%s]: "x, __func__, ##__VA_ARGS__)
+do {	\
+	if (ilc) \
+		ipc_log_string(ilc, "[%s]: "x, __func__, ##__VA_ARGS__); \
+} while (0)
 
 static void qcom_smp2p_kick(struct qcom_smp2p *smp2p)
 {
