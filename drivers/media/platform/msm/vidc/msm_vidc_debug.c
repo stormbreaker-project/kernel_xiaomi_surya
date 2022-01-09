a5ea526a578d49fb08e6fee12d5de21455b6f88e
Forenche <prahul2003@gmail.com>
2022-01-09 22:40:18 +0530

    Revert "vidc: stub out msm_vidc_debug"
    This reverts commit d5e2d7cf24270ba790d725693395f51eae79e68f.
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/media/platform/msm/vidc/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.c
@@ -32,7 +32,6 @@ bool msm_vidc_thermal_mitigation_disabled = !true;
 int msm_vidc_clock_voting = !1;
 bool msm_vidc_syscache_disable = !true;
 
-#if 0
 #define MAX_DBG_BUF_SIZE 4096
 
 #define DYNAMIC_BUF_OWNER(__binfo) ({ \
@@ -604,4 +603,3 @@ int msm_vidc_check_ratelimit(void)
 	return __ratelimit(&_rs);
 }
 
-#endif
