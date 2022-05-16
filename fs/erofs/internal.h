791a20fee188234cc0126a2824a436b926028f16
John Galt <johngaltfirstrun@gmail.com>
2022-05-16 09:41:14 -0400

    Revert "erofs: fixes for compilation"
    This reverts commit c7bf11979051cda0e7b37857289503fa4831c549.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- fs/erofs/internal.h
+++ fs/erofs/internal.h
@@ -136,6 +136,35 @@ static inline bool erofs_workgroup_try_to_freeze(struct erofs_workgroup *grp,
 						 int val)
 {
 	preempt_disable();
+	if (val != atomic_cmpxchg(&grp->refcount, val, EROFS_LOCKED_MAGIC)) {
+		preempt_enable();
+		return false;
+	}
+	return true;
+}
+
+static inline void erofs_workgroup_unfreeze(struct erofs_workgroup *grp,
+					    int orig_val)
+{
+	/*
+	 * other observers should notice all modifications
+	 * in the freezing period.
+	 */
+	smp_mb();
+	atomic_set(&grp->refcount, orig_val);
+	preempt_enable();
+}
+
+static inline int erofs_wait_on_workgroup_freezed(struct erofs_workgroup *grp)
+{
+	return atomic_cond_read_relaxed(&grp->refcount,
+					VAL != EROFS_LOCKED_MAGIC);
+}
+#else
+static inline bool erofs_workgroup_try_to_freeze(struct erofs_workgroup *grp,
+						 int val)
+{
+	preempt_disable();
 	/* no need to spin on UP platforms, let's just disable preemption. */
 	if (val != atomic_read(&grp->refcount)) {
 		preempt_enable();
