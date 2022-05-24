3803a5e49b585e17efc9e047dab74e91878d20a2
KenHV <yo@kenharris.xyz>
2022-05-24 14:26:28 +0530

    Revert "sched/core: Use SCHED_RR in place of SCHED_FIFO for all users"
    This reverts commit a091779021bc06eaca3698eff39961d9f374e975.
    
    Geekbench multi-core score regresses by 100.
    
    Signed-off-by: KenHV <yo@kenharris.xyz>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4331,8 +4331,7 @@ static void __setscheduler_params(struct task_struct *p,
 	else
 		policy &= ~SCHED_RESET_ON_FORK;
 
-	/* Replace SCHED_FIFO with SCHED_RR to reduce latency */
-	p->policy = policy == SCHED_FIFO ? SCHED_RR : policy;
+	p->policy = policy;
 
 	if (dl_policy(policy))
 		__setparam_dl(p, attr);
