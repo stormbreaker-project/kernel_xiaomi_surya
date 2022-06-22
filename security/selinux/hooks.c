0f0fb75094aed45632eb0ef97e8ae4add446a16e
Forenche <prahul2003@gmail.com>
2022-06-22 13:40:34 +0530

    Revert "Revert "selinux: Don't require auditing""
    This reverts commit 335b713387227b35aac72e6ad315d7953c1e9bb9.
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3164,6 +3164,7 @@ static noinline int audit_inode_permission(struct inode *inode,
 					   int result,
 					   unsigned flags)
 {
+#ifdef CONFIG_AUDIT
 	struct common_audit_data ad;
 	struct inode_security_struct *isec = inode->i_security;
 	int rc;
@@ -3176,6 +3177,7 @@ static noinline int audit_inode_permission(struct inode *inode,
 			    audited, denied, result, &ad, flags);
 	if (rc)
 		return rc;
+#endif
 	return 0;
 }
 
--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3563,6 +3563,7 @@ out:
 	return match;
 }
 
+#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3585,6 +3586,7 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
+#endif
 
 #ifdef CONFIG_NETLABEL
 /**
