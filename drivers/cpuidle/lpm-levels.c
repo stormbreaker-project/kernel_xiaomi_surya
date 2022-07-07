5866bf768724096f48eb320f8095775eed4b8945
Forenche <prahul2003@gmail.com>
2022-07-07 23:46:36 +0530

    Revert "lpm-levels: Allow CPU to idle when executing audio code"
    *Increases power consumption while playing audio in idle.
    
    This reverts commit 01fadc2397711316f777177090c17e52bc2a2909.
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -120,7 +120,7 @@ module_param_named(sleep_disabled, sleep_disabled, bool, 0664);
  */
 s32 msm_cpuidle_get_deep_idle_latency(void)
 {
-	return 100;
+	return 10;
 }
 EXPORT_SYMBOL(msm_cpuidle_get_deep_idle_latency);
 
